6538d72a384843bf5c45184c1c67f0b8e40425e5
Dag Brattli <dag@brattli.net>
2014-07-20 19:22:23 +0200

    AnonymousObservable refactoring

--- rx/anonymousobservable.py
+++ rx/anonymousobservable.py
@@ -1,12 +1,9 @@
 import types
-import logging
 
 from rx.concurrency import current_thread_scheduler
 from rx.disposables import Disposable
 from .autodetachobserver import AutoDetachObserver
 from .observable import Observable
-
-log = logging.getLogger("Rx")
 
 class AnonymousObservable(Observable):
     """Class to create an Observable instance from a delegate-based 
@@ -31,27 +28,20 @@ class AnonymousObservable(Observable):
         subscribe -- Subscribe method implementation.
         """
         def _subscribe(observer):
-            auto_detach_observer = AutoDetachObserver(observer)
-
-            # Backup. Kick queue if there's anything left: TODO: can we move this somewhere else?
-            if current_thread_scheduler.schedule_required():
-                def action(scheduler, state=None):
-                    try:
-                        auto_detach_observer.disposable = self.fix_subscriber(subscribe(auto_detach_observer))
-                    except Exception as ex:
-                        if not auto_detach_observer.fail(ex):
-                            raise ex
-                current_thread_scheduler.schedule(action)
-            else:
+            def set_disposable():
                 try:
                     auto_detach_observer.disposable = self.fix_subscriber(subscribe(auto_detach_observer))
                 except Exception as ex:
-                    log.error("AnonymousObservable Exception: %s" % ex)
-                    
                     if not auto_detach_observer.fail(ex):
                         raise ex
+
+            auto_detach_observer = AutoDetachObserver(observer)
+
+            if current_thread_scheduler.schedule_required():
+                current_thread_scheduler.schedule(set_disposable)
+            else:
+                set_disposable()
 
             return auto_detach_observer
         
         super(AnonymousObservable, self).__init__(_subscribe)
-    
