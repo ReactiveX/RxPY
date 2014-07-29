9d98f800ff362ebf45814c21e57fb7a32405d03f
Dag Brattli <dag@brattli.net>
2014-07-29 12:01:46 +0000

    Moved more tests

---
+++ tests/test_observable/test_fromarray.py
@@ -0,0 +1,50 @@
+import unittest
+
+from rx import Observable
+from rx.testing import TestScheduler, ReactiveTest, is_prime, MockDisposable
+from rx.disposables import Disposable, SerialDisposable, BooleanDisposable
+
+on_next = ReactiveTest.on_next
+on_completed = ReactiveTest.on_completed
+on_error = ReactiveTest.on_error
+subscribe = ReactiveTest.subscribe
+subscribed = ReactiveTest.subscribed
+disposed = ReactiveTest.disposed
+created = ReactiveTest.created
+
+class RxException(Exception):
+    pass
+
+# Helper function for raising exceptions within lambdas
+def _raise(ex):
+    raise RxException(ex)
+
+class TestFromArray(unittest.TestCase):
+    def test_subscribe_to_enumerable_finite(self):
+        enumerable_finite = [1, 2, 3, 4, 5]
+        scheduler = TestScheduler()
+
+        def create():
+            return Observable.from_array(enumerable_finite, scheduler=scheduler)
+
+        results = scheduler.start(create)
+
+        results.messages.assert_equal(
+                            on_next(201, 1),
+                            on_next(202, 2),
+                            on_next(203, 3),
+                            on_next(204, 4),
+                            on_next(205, 5),
+                            on_completed(206)
+                        )
+
+    def test_subscribe_to_enumerable_empty(self):
+      enumerable_finite = []
+
+      scheduler = TestScheduler()
+
+      def create():
+          return Observable.from_array(enumerable_finite, scheduler=scheduler)
+      results = scheduler.start(create)
+
+      results.messages.assert_equal(on_completed(201))
---
+++ tests/test_observable/test_range.py
@@ -0,0 +1,58 @@
+import unittest
+
+from rx import Observable
+from rx.testing import TestScheduler, ReactiveTest, is_prime, MockDisposable
+from rx.disposables import Disposable, SerialDisposable, BooleanDisposable
+
+on_next = ReactiveTest.on_next
+on_completed = ReactiveTest.on_completed
+on_error = ReactiveTest.on_error
+subscribe = ReactiveTest.subscribe
+subscribed = ReactiveTest.subscribed
+disposed = ReactiveTest.disposed
+created = ReactiveTest.created
+
+class TestRange(unittest.TestCase):
+    def test_range_zero(self):
+        scheduler = TestScheduler()
+    
+        def create():
+            return Observable.range(0, 0, scheduler)
+    
+        results = scheduler.start(create)
+        results.messages.assert_equal(on_completed(201))
+    
+    def test_range_one(self):
+        scheduler = TestScheduler()
+    
+        def create():
+            return Observable.range(0, 1, scheduler)
+        results = scheduler.start(create)
+    
+        results.messages.assert_equal(on_next(201, 0), on_completed(202))
+    
+    def test_range_five(self):
+        scheduler = TestScheduler()
+    
+        def create():
+            return Observable.range(10, 5, scheduler)
+    
+        results = scheduler.start(create)
+    
+        results.messages.assert_equal(
+                            on_next(201, 10),
+                            on_next(202, 11),
+                            on_next(203, 12),
+                            on_next(204, 13),
+                            on_next(205, 14),
+                            on_completed(206))
+    
+    def test_range_dispose(self):
+        scheduler = TestScheduler()
+    
+        def create():
+            return Observable.range(-10, 5, scheduler)
+    
+        results = scheduler.start(create, disposed=204)
+        results.messages.assert_equal(on_next(201, -10), on_next(202, -9), on_next(203, -8))
+    
