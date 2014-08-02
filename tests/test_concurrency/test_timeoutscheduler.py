c82e4c51c8d6d15f26ff2e4d18a0ddcb49e8a1d6
Dag Brattli <dag@brattli.net>
2014-08-02 17:51:40 +0000

    Cleanup

--- tests/test_concurrency/test_timeoutscheduler.py
+++ tests/test_concurrency/test_timeoutscheduler.py
@@ -1,3 +1,5 @@
+import unittest
+
 from datetime import datetime, timedelta
 from time import sleep
 from rx.concurrency import TimeoutScheduler
@@ -6,41 +8,41 @@ def test_timeout_now():
     res = TimeoutScheduler.now() - datetime.utcnow()
     assert res < timedelta(microseconds=1000)
 
-
-def test_timeout_schedule_action():
-    scheduler = TimeoutScheduler()
-    ran = [False]
+class TestTimeoutScheduler(unittest.TestCase):
+    def test_timeout_schedule_action(self):
+        scheduler = TimeoutScheduler()
+        ran = [False]
     
-    def action(scheduler, state):
-        ran[0] = True
-
-    scheduler.schedule(action)
-
-    sleep(0.1)
-    assert (ran[0] == True)
-
-def test_thread_pool_schedule_action_due():
-    scheduler = TimeoutScheduler()
-    starttime = datetime.utcnow()
-    endtime = [None]
+        def action(scheduler, state):
+            ran[0] = True
     
-    def action(scheduler, state):
-        endtime[0] = datetime.utcnow()
+        scheduler.schedule(action)
     
-    scheduler.schedule_relative(timedelta(milliseconds=200), action)
-
-    sleep(0.3)
-    diff = endtime[0]-starttime
-    assert(diff > timedelta(milliseconds=180))
+        sleep(0.1)
+        assert (ran[0] == True)
     
-def test_timeout_schedule_action_cancel():
-    ran = False
-    scheduler = TimeoutScheduler()
-
-    def action(scheduler, state):
-        ran = True
-    d = scheduler.schedule_relative(timedelta(milliseconds=1), action)
-    d.dispose()
-
-    sleep(0.1)
-    assert (not ran)
+    def test_timeout_schedule_action_due(self):
+        scheduler = TimeoutScheduler()
+        starttime = datetime.utcnow()
+        endtime = [None]
+    
+        def action(scheduler, state):
+            endtime[0] = datetime.utcnow()
+    
+        scheduler.schedule_relative(timedelta(milliseconds=200), action)
+    
+        sleep(0.3)
+        diff = endtime[0]-starttime
+        assert(diff > timedelta(milliseconds=180))
+    
+    def test_timeout_schedule_action_cancel(self):
+        ran = False
+        scheduler = TimeoutScheduler()
+    
+        def action(scheduler, state):
+            ran = True
+        d = scheduler.schedule_relative(timedelta(milliseconds=1), action)
+        d.dispose()
+    
+        sleep(0.1)
+        assert (not ran)
