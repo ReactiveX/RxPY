c37271f72dec4f4e51604617f3abf847fc39146c
Dag Brattli <dag@brattli.net>
2014-08-02 04:31:32 +0000

    Added tests for min and max

--- tests/test_observable/test_min.py
+++ tests/test_observable/test_min.py
@@ -16,7 +16,7 @@ class TestMin(unittest.TestCase):
     def test_min_int32_empty(self):
         scheduler = TestScheduler()
         xs = scheduler.create_hot_observable(on_next(150, 1), on_completed(250))
-        
+
         def create():
             return xs.min()
         res = scheduler.start(create=create).messages
@@ -49,94 +49,94 @@ class TestMin(unittest.TestCase):
         res = scheduler.start(create=lambda: xs.min()).messages
         res.assert_equal()
 
-# def test_MinOfT_Comparer_Empty():
-#     var comparer, res, scheduler, xs
-#     scheduler = TestScheduler()
-#     comparer = function (a, b) {
-#         if (a > b) {
-#             return -1
-#         }
-#         if (a == b) {
-#             return 0
-#         }
-#         return 1
-#     }
-#     xs = scheduler.create_hot_observable(on_next(150, 'a'), on_completed(250))
-#     res = scheduler.start(create=create)
-#         return xs.min(comparer)
-#     }).messages
-#     equal(1, res.length)
-#     ok(res[0].value.kind == 'E' and res[0].value.exception != null)
-#     ok(res[0].time == 250)
+    def test_min_of_t_comparer_empty(self):
+        scheduler = TestScheduler()
+        def comparer(a, b):
+            if a > b:
+                return -1
 
+            if a == b:
+                return 0
 
-# def test_MinOfT_Comparer_Empty():
-#     var comparer, res, scheduler, xs
-#     scheduler = TestScheduler()
-#     comparer = function (a, b) {
-#         if (a > b) {
-#             return -1
-#         }
-#         if (a == b) {
-#             return 0
-#         }
-#         return 1
-#     }
-#     xs = scheduler.create_hot_observable(on_next(150, 'z'), on_next(210, "b"), on_next(220, "c"), on_next(230, "a"), on_completed(250))
-#     res = scheduler.start(create=create)
-#         return xs.min(comparer)
-#     }).messages
-#     res.assert_equal(on_next(250, "c"), on_completed(250))
+            return 1
 
+        xs = scheduler.create_hot_observable(on_next(150, 'a'), on_completed(250))
 
-# def test_MinOfT_Comparer_Throw():
-#     var comparer, ex, res, scheduler, xs
-#     ex = 'ex'
-#     scheduler = TestScheduler()
-#     comparer = function (a, b) {
-#         if (a > b) {
-#             return -1
-#         }
-#         if (a == b) {
-#             return 0
-#         }
-#         return 1
-#     }
-#     xs = scheduler.create_hot_observable(on_next(150, 'z'), on_error(210, ex))
-#     res = scheduler.start(create=create)
-#         return xs.min(comparer)
-#     }).messages
-#     res.assert_equal(on_error(210, ex))
+        def create():
+            return xs.min(comparer)
 
+        res = scheduler.start(create=create).messages
+        self.assertEqual(1, len(res))
+        assert(res[0].value.kind == 'E' and res[0].value.exception != None)
+        assert(res[0].time == 250)
 
-# def test_MinOfT_Comparer_Never():
-#     var comparer, res, scheduler, xs
-#     scheduler = TestScheduler()
-#     comparer = function (a, b) {
-#         if (a > b) {
-#             return -1
-#         }
-#         if (a == b) {
-#             return 0
-#         }
-#         return 1
-#     }
-#     xs = scheduler.create_hot_observable(on_next(150, 'z'))
-#     res = scheduler.start(create=create)
-#         return xs.min(comparer)
-#     }).messages
-#     res.assert_equal()
+    def test_min_of_t_comparer_empty(self):
+        scheduler = TestScheduler()
+        def comparer(a, b):
+            if a > b:
+                return -1
 
+            if a == b:
+                return 0
 
-# def test_MinOfT_ComparerThrows():
-#     var comparer, ex, res, scheduler, xs
-#     ex = 'ex'
-#     scheduler = TestScheduler()
-#     comparer = function (a, b) {
-#         throw ex
-#     }
-#     xs = scheduler.create_hot_observable(on_next(150, 'z'), on_next(210, "b"), on_next(220, "c"), on_next(230, "a"), on_completed(250))
-#     res = scheduler.start(create=create)
-#         return xs.min(comparer)
-#     }).messages
-#     res.assert_equal(on_error(220, ex))
+            return 1
+
+        xs = scheduler.create_hot_observable(on_next(150, 'z'), on_next(210, "b"), on_next(220, "c"), on_next(230, "a"), on_completed(250))
+
+        def create():
+            return xs.min(comparer)
+
+        res = scheduler.start(create=create).messages
+        res.assert_equal(on_next(250, "c"), on_completed(250))
+
+    def test_min_of_t_comparer_throw(self):
+        ex = 'ex'
+        scheduler = TestScheduler()
+        def comparer(a, b):
+            if a > b:
+                return -1
+            
+            if a == b:
+                return 0
+            
+            return 1
+        
+        xs = scheduler.create_hot_observable(on_next(150, 'z'), on_error(210, ex))
+        
+        def create():
+            return xs.min(comparer)
+        
+        res = scheduler.start(create=create).messages
+        res.assert_equal(on_error(210, ex))
+    
+    def test_min_of_t_comparer_never(self):
+        scheduler = TestScheduler()
+        def comparer(a, b):
+            if a > b:
+                return -1
+            
+            if a == b:
+                return 0
+            
+            return 1
+        
+        xs = scheduler.create_hot_observable(on_next(150, 'z'))
+        
+        def create():
+            return xs.min(comparer)
+        res = scheduler.start(create=create).messages
+        res.assert_equal()
+
+    def test_min_of_t_comparer_throws(self):
+        ex = 'ex'
+        scheduler = TestScheduler()
+        def comparer(a, b):
+            raise Exception(ex)
+        
+        xs = scheduler.create_hot_observable(on_next(150, 'z'), on_next(210, "b"), on_next(220, "c"), on_next(230, "a"), on_completed(250))
+        
+        def create():
+            return xs.min(comparer)
+        
+        res = scheduler.start(create=create).messages
+        res.assert_equal(on_error(220, ex))
