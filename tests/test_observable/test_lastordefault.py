afafa177020c1a75618c4939bcc93dae5a551599
Dag Brattli <dag@brattli.net>
2014-08-05 18:13:41 +0000

    Cleanup

--- tests/test_observable/test_lastordefault.py
+++ tests/test_observable/test_lastordefault.py
@@ -19,7 +19,7 @@ class RxException(Exception):
 def _raise(ex):
     raise RxException(ex)
 
-class TestLast_or_default(unittest.TestCase):
+class TestLastOrDefault(unittest.TestCase):
     def test_last_or_default_async_empty(self):
         scheduler = TestScheduler()
         xs = scheduler.create_hot_observable(on_next(150, 1), on_completed(250))
@@ -119,18 +119,18 @@ class TestLast_or_default(unittest.TestCase):
         ex = 'ex'
         scheduler = TestScheduler()
         xs = scheduler.create_hot_observable(on_next(150, 1), on_next(210, 2), on_next(220, 3), on_next(230, 4), on_next(240, 5), on_completed(250))
-        
+
         def create():
             def predicate(x):
                 if x < 4:
                     return x % 2 == 1
                 else:
                     raise Exception(ex)
-                
+
             return xs.last_or_default(predicate, 0)
-    
+
         res = scheduler.start(create=create)
-        
+
         res.messages.assert_equal(on_error(230, ex))
         xs.subscriptions.assert_equal(subscribe(200, 230))
-    
+
