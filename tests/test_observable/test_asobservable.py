db5ec359e20d31ee8a4e72f57af0a83cddb40b64
Dag Brattli <dag@brattli.net>
2014-08-22 06:11:20 +0000

    Added more tests

--- tests/test_observable/test_asobservable.py
+++ tests/test_observable/test_asobservable.py
@@ -17,63 +17,70 @@ class TestAsObservable(unittest.TestCase):
         some_observable = Observable.empty()
         assert(some_observable.as_observable() != some_observable)
 
-# def test_as_observable_Never():
-#     var results, scheduler
-#     scheduler = TestScheduler()
-#     results = scheduler.start(create)
-#         return Rx.Observable.never().as_observable()
+    def test_as_observable_never(self):
+        scheduler = TestScheduler()
 
-#     results.messages.assert_equal()
+        def create():
+            return Observable.never().as_observable()
+        results = scheduler.start(create)
 
-# def test_as_observable_Empty():
-#     var results, scheduler, xs
-#     scheduler = TestScheduler()
-#     xs = scheduler.create_hot_observable(on_next(150, 1), on_completed(250))
-#     results = scheduler.start(create)
-#         return xs.as_observable()
-#     }).messages
-#     equal(1, results.length)
-#     assert(results[0].value.kind == 'C' and results[0].time == 250)
+        results.messages.assert_equal()
 
-# def test_as_observable_Throw():
-#     var ex, results, scheduler, xs
-#     ex = 'ex'
-#     scheduler = TestScheduler()
-#     xs = scheduler.create_hot_observable(on_next(150, 1), on_error(250, ex))
-#     results = scheduler.start(create)
-#         return xs.as_observable()
-#     }).messages
-#     equal(1, results.length)
-#     assert(results[0].value.kind == 'E' and results[0].value.exception == ex and results[0].time == 250)
+    def test_as_observable_empty(self):
+        scheduler = TestScheduler()
+        xs = scheduler.create_hot_observable(on_next(150, 1), on_completed(250))
 
-# def test_as_observable_Return():
-#     var results, scheduler, xs
-#     scheduler = TestScheduler()
-#     xs = scheduler.create_hot_observable(on_next(150, 1), on_next(220, 2), on_completed(250))
-#     results = scheduler.start(create)
-#         return xs.as_observable()
-#     }).messages
-#     equal(2, results.length)
-#     assert(results[0].value.kind == 'N' and results[0].value.value == 2 and results[0].time == 220)
-#     assert(results[1].value.kind == 'C' and results[1].time == 250)
+        def create():
+            return xs.as_observable()
 
-# def test_as_observable_IsNotEager():
-#     var scheduler, subscribed, xs
-#     scheduler = TestScheduler()
-#     subscribed = false
-#     xs = Rx.Observable.create(function (obs) {
-#         var disp
-#         subscribed = true
-#         disp = scheduler.create_hot_observable(on_next(150, 1), on_next(220, 2), on_completed(250)).subscribe(obs)
-#         return function () {
-#             return disp.dispose()
-#         }
+        results = scheduler.start(create).messages
+        self.assertEqual(1, len(results))
+        assert(results[0].value.kind == 'C' and results[0].time == 250)
 
-#     xs.as_observable()
-#     assert(!subscribed)
-#     scheduler.start(create)
-#         return xs.as_observable()
+    def test_as_observable_throw(self):
+        ex = 'ex'
+        scheduler = TestScheduler()
+        xs = scheduler.create_hot_observable(on_next(150, 1), on_error(250, ex))
 
-#     assert(subscribed)
+        def create():
+            return xs.as_observable()
+
+        results = scheduler.start(create).messages
+        self.assertEqual(1, len(results))
+        assert(results[0].value.kind == 'E' and results[0].value.exception == ex and results[0].time == 250)
+
+    def test_as_observable_Return(self):
+        scheduler = TestScheduler()
+        xs = scheduler.create_hot_observable(on_next(150, 1), on_next(220, 2), on_completed(250))
+
+        def create():
+            return xs.as_observable()
+
+        results = scheduler.start(create).messages
+        self.assertEqual(2, len(results))
+        assert(results[0].value.kind == 'N' and results[0].value.value == 2 and results[0].time == 220)
+        assert(results[1].value.kind == 'C' and results[1].time == 250)
+
+    def test_as_observable_isnoteager(self):
+        scheduler = TestScheduler()
+        subscribed = [False]
+        
+        def subscribe(obs):
+            subscribed[0] = True
+            disp = scheduler.create_hot_observable(on_next(150, 1), on_next(220, 2), on_completed(250)).subscribe(obs)
+
+            def func():
+                return disp.dispose()
+            return func
+
+        xs = Observable.create(subscribe)
+        xs.as_observable()
+        assert(not subscribed[0])
+
+        def create():
+            return xs.as_observable()
+        scheduler.start(create)
+
+        assert(subscribed[0])
 
 
