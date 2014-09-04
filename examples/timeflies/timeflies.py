65f65eaa2c1ebe827b62475553eecf44cca716b4
Dag Brattli <dag@brattli.net>
2014-09-04 23:07:27 +0200

    Tabs to spaces

--- examples/timeflies/timeflies.py
+++ examples/timeflies/timeflies.py
@@ -4,31 +4,31 @@ from rx.concurrency import TkinterScheduler
 from tkinter import *
 
 def main():
-	root = Tk()
-	root.title("Rx for Python rocks")
-	scheduler = TkinterScheduler(root)
+    root = Tk()
+    root.title("Rx for Python rocks")
+    scheduler = TkinterScheduler(root)
 
-	mousemove = Subject()
+    mousemove = Subject()
 
-	frame = Frame(root, width=600, height=600)
+    frame = Frame(root, width=600, height=600)
 
-	def move(event):
-		mousemove.on_next(event)
-	frame.bind("<Motion>", move)
+    def move(event):
+        mousemove.on_next(event)
+    frame.bind("<Motion>", move)
 
-	text = 'TIME FLIES LIKE AN ARROW'
-	labels = [Label(frame, text=c) for c in text]
+    text = 'TIME FLIES LIKE AN ARROW'
+    labels = [Label(frame, text=c) for c in text]
 
-	def handle_label(i, label):
-		def on_next(ev):
-			label.place(x=ev.x + i*12 + 15, y=ev.y)
-		mousemove.delay(i*100, scheduler=scheduler).subscribe(on_next)
-		
-	for i, label in enumerate(labels):
-		handle_label(i, label)
-		
-	frame.pack()
-	root.mainloop()
+    def handle_label(i, label):
+        def on_next(ev):
+            label.place(x=ev.x + i*12 + 15, y=ev.y)
+        mousemove.delay(i*100, scheduler=scheduler).subscribe(on_next)
+        
+    for i, label in enumerate(labels):
+        handle_label(i, label)
+        
+    frame.pack()
+    root.mainloop()
 
 if __name__ == '__main__':
-	main()
+    main()
