da6e58b42fc5d5b8be586d1a7a606bd235f8538c
Dag Brattli <dag@brattli.net>
2014-09-11 06:30:09 +0000

    Fix to handle time as both datetime and ints

--- rx/linq/observable/delay.py
+++ rx/linq/observable/delay.py
@@ -22,7 +22,7 @@ class ObservableDelay(Observable):
 
     def observable_delay_timespan(self, duetime, scheduler):
         source = self
-        
+
         def subscribe(observer):
             cancelable = SerialDisposable()
             exception = [None]
@@ -44,7 +44,7 @@ class ObservableDelay(Observable):
                     q.append(Timestamp(value=notification.value, timestamp=notification.timestamp + duetime))
                     should_run = not active[0]
                     active[0] = True
-                
+
                 if should_run:
                     if exception[0]:
                         log.error("*** Exception: %s", exception[0])
@@ -57,19 +57,19 @@ class ObservableDelay(Observable):
                             if exception[0]:
                                 log.error("observable_delay_timespan:subscribe:on_next:action(), exception: %s", exception[0])
                                 return
-                            
+
                             running[0] = True
                             while True:
                                 result = None
                                 if len(q) and q[0].timestamp <= scheduler.now():
                                     result = q.pop(0).value
-                                
+
                                 if result:
                                     result.accept(observer)
-                                
+
                                 if not result:
                                     break
-                            
+
                             should_recurse = False
                             recurse_duetime = 0
                             if len(q) :
@@ -79,7 +79,7 @@ class ObservableDelay(Observable):
                                 recurse_duetime = max(zero, diff)
                             else:
                                 active[0] = False
-                            
+
                             ex = exception[0]
                             running[0] = False
                             if ex:
@@ -94,28 +94,28 @@ class ObservableDelay(Observable):
 
     def observable_delay_date(self, duetime, scheduler):
         def defer():
-            timespan = duetime - scheduler.now()
+            timespan = scheduler.normalize(duetime) - scheduler.now()
             return self.observable_delay_timespan(timespan, scheduler)
-        
+
         return Observable.defer(defer)
 
     def delay(self, duetime, scheduler=None):
-        """Time shifts the observable sequence by duetime. The relative time 
+        """Time shifts the observable sequence by duetime. The relative time
         intervals between the values are preserved.
-        
+
         1 - res = rx.Observable.delay(datetime())
         2 - res = rx.Observable.delay(datetime(), Scheduler.timeout)
-        
+
         3 - res = rx.Observable.delay(5000)
         4 - res = rx.Observable.delay(5000, Scheduler.timeout)
-        
+
         Keyword arguments:
-        duetime -- Absolute (specified as a datetime object) or relative time 
-            (specified as an integer denoting milliseconds) by which to shift 
+        duetime -- Absolute (specified as a datetime object) or relative time
+            (specified as an integer denoting milliseconds) by which to shift
             the observable sequence.
-        scheduler -- [Optional] Scheduler to run the delay timers on. If not 
+        scheduler -- [Optional] Scheduler to run the delay timers on. If not
             specified, the timeout scheduler is used.
-        
+
         Returns time-shifted sequence.
         """
         scheduler = scheduler or timeout_scheduler
--- rx/subjects/replaysubject.py
+++ rx/subjects/replaysubject.py
@@ -20,28 +20,26 @@ class RemovableDisposable(object):
         self.observer.dispose()
         if not self.subject.is_disposed and self.observer in self.subject.observers:
             self.subject.observers.remove(self.observer)
-            
+
 # Replay Subject
 class ReplaySubject(Observable, AbstractObserver):
-    """Represents an object that is both an observable sequence as well as an 
-    observer. Each notification is broadcasted to all subscribed and future 
+    """Represents an object that is both an observable sequence as well as an
+    observer. Each notification is broadcasted to all subscribed and future
     observers, subject to buffer trimming policies.
-    """ 
-    
+    """
+
     def __init__(self, buffer_size=None, window=None, scheduler=None):
-        """Initializes a new instance of the ReplaySubject class with the 
+        """Initializes a new instance of the ReplaySubject class with the
         specified buffer size, window and scheduler.
-     
+
         Keyword arguments:
         buffer_size -- [Optional] Maximum element count of the replay buffer.
         window [Optional] -- Maximum time length of the replay buffer.
         scheduler -- [Optional] Scheduler the observers are invoked on.
         """
-    
+
         self.buffer_size = sys.maxsize if buffer_size is None else buffer_size
-        self.window = timedelta.max if window is None else window
-        if not isinstance(self.window, timedelta):
-            self.window = timedelta(milliseconds=self.window)
+        self.window = scheduler.normalize(timedelta.max) if window is None else window
         self.scheduler = scheduler or current_thread_scheduler
         self.q = []
         self.observers = []
@@ -77,11 +75,11 @@ class ReplaySubject(Observable, AbstractObserver):
 
         so.ensure_active() # n)
         return subscription
-    
+
     def _trim(self, now):
         while len(self.q) > self.buffer_size:
             self.q.pop(0)
-        
+
         while len(self.q) > 0 and (now - self.q[0]['interval']) > self.window:
             self.q.pop(0)
 
@@ -95,7 +93,7 @@ class ReplaySubject(Observable, AbstractObserver):
             for observer in self.observers[:]:
                 observer.on_next(value)
                 observer.ensure_active()
-    
+
     def on_error(self, error):
         self.check_disposed()
         if not self.is_stopped:
@@ -104,13 +102,13 @@ class ReplaySubject(Observable, AbstractObserver):
             self.has_error = True
             now = self.scheduler.now()
             self._trim(now)
-            
+
             for observer in self.observers[:]:
                 observer.on_error(error)
                 observer.ensure_active()
-            
+
             self.observers = []
-        
+
     def on_completed(self):
         self.check_disposed()
         if not self.is_stopped:
@@ -120,10 +118,10 @@ class ReplaySubject(Observable, AbstractObserver):
             for observer in self.observers[:]:
                 observer.on_completed()
                 observer.ensure_active()
-            
+
             self.observers = []
-        
+
     def dispose(self):
         self.is_disposed = True
         self.observers = None
-    
+
