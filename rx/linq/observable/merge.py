3ebeef44eee916bdb514eea01a43b728ed5c8edf
Dag Brattli <dag@brattli.net>
2014-09-21 17:16:45 +0000

    More bugfixes for merge

--- rx/linq/observable/merge.py
+++ rx/linq/observable/merge.py
@@ -11,7 +11,7 @@ class ObservableMerge(Observable):
     def __init__(self, subscribe):
         self.merge = self.__merge
 
-    def __merge(self, max_concurrent_or_other):
+    def __merge(self, *args, **kwargs):
         """Merges an observable sequence of observable sequences into an
         observable sequence, limiting the number of concurrent subscriptions
         to inner sequences. Or merges two observable sequences into a single
@@ -27,9 +27,11 @@ class ObservableMerge(Observable):
         Returns the observable sequence that merges the elements of the inner
         sequences."""
 
-        if not isinstance(max_concurrent_or_other, int):
-            return Observable.merge(self, max_concurrent_or_other)
+        if not isinstance(args[0], int):
+            args = args + tuple([self])
+            return Observable.merge(*args, **kwargs)
 
+        max_concurrent = args[0]
         sources = self
 
         def subscribe(observer):
@@ -44,8 +46,8 @@ class ObservableMerge(Observable):
 
                 def on_completed():
                     group.remove(subscription)
-                    if q.length > 0:
-                        s = q.shift()
+                    if len(q):
+                        s = q.pop(0)
                         subscribe(s)
                     else:
                         active_count[0] -= 1
@@ -55,7 +57,7 @@ class ObservableMerge(Observable):
                 subscription.disposable = xs.subscribe(observer.on_next, observer.on_error, on_completed)
 
             def on_next(inner_source):
-                if active_count[0] < max_concurrent_or_other:
+                if active_count[0] < max_concurrent:
                     active_count[0] += 1
                     subscribe(inner_source)
                 else:
@@ -96,7 +98,7 @@ class ObservableMerge(Observable):
 
         if isinstance(sources[0], list):
             sources = sources[0]
-
+        
         return Observable.from_array(sources, scheduler).merge_observable()
 
     def merge_all(self):
