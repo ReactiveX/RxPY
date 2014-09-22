5db4817414357c8fa3197a9f62268f507a6766aa
Dag Brattli <dag@brattli.net>
2014-09-22 13:03:02 +0200

    Use single underscore for private methods

--- rx/subjects/asyncsubject.py
+++ rx/subjects/asyncsubject.py
@@ -16,7 +16,7 @@ class AsyncSubject(Observable, AbstractObserver):
         cached for all future observations.
         """
 
-        super(AsyncSubject, self).__init__(self.__subscribe)
+        super(AsyncSubject, self).__init__(self._subscribe)
 
         self.is_disposed = False
         self.is_stopped = False
@@ -29,7 +29,7 @@ class AsyncSubject(Observable, AbstractObserver):
         if self.is_disposed:
             raise DisposedException()
 
-    def __subscribe(self, observer):
+    def _subscribe(self, observer):
         self.check_disposed()
         if not self.is_stopped:
             self.observers.append(observer)
