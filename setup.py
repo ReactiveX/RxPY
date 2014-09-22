cccddf4ef82ba5f93fd4df3e26757b9122160e08
Dag Brattli <dag@brattli.net>
2014-09-22 17:39:32 +0000

    Bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='Rx',
-    version='0.10.2',
+    version='0.10.3',
     description='Reactive Extensions for Python',
     long_description = """\
 is a library for composing asynchronous and event-based programs using observable collections and LINQ-style query operators in Python""",
