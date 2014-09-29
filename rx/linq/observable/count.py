ac226dbc55dc0a707e03182e489401f5ac8619f9
Dag Brattli <dag@brattli.net>
2014-09-29 19:02:14 +0000

    __len__ should return int

--- rx/linq/observable/count.py
+++ rx/linq/observable/count.py
@@ -5,23 +5,23 @@ from rx.internal import ExtensionMethod
 
 @add_metaclass(ExtensionMethod)
 class ObservableCount(Observable):
-    """Note that we do some magic here by using a meta class to extend 
+    """Note that we do some magic here by using a meta class to extend
     Observable with the methods in this class"""
 
     def count(self, predicate=None):
-        """Returns an observable sequence containing a value that represents 
-        how many elements in the specified observable sequence satisfy a 
+        """Returns an observable sequence containing a value that represents
+        how many elements in the specified observable sequence satisfy a
         condition if provided, else the count of items.
-     
+
         1 - res = source.count()
         2 - res = source.count(lambda x: x > 3)
-    
+
         Keyword arguments:
         predicate -- [Optional] A function to test each element for a condition.
-     
-        Returns an observable sequence containing a single element with a 
-        number that represents how many elements in the input sequence satisfy 
-        the condition in the predicate function if provided, else the count of 
+
+        Returns an observable sequence containing a single element with a
+        number that represents how many elements in the input sequence satisfy
+        the condition in the predicate function if provided, else the count of
         items in the sequence.
         """
 
@@ -29,9 +29,3 @@ class ObservableCount(Observable):
             return self.where(predicate).count()
         else:
             return self.aggregate(lambda count, _: count + 1, seed=0)
-
-    
-    def __len__(self):
-        """Pythonc version of count"""
-        
-        return self.count()
