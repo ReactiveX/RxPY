5800ceabe0057a2cd45155b38b8f478e2a3f7fe6
Dag Brattli <dag@brattli.net>
2014-10-01 19:51:23 +0000

    Whitespace

--- rx/linq/observable/catch.py
+++ rx/linq/observable/catch.py
@@ -23,59 +23,59 @@ class ObservableCatch(Observable):
             subscription = SerialDisposable()
 
             subscription.disposable = d1
-            
+
             def on_error(exception):
                 try:
                     result = handler(exception)
                 except Exception as ex:
                     observer.on_error(ex)
                     return
-                
+
                 result = Observable.from_future(result)
                 d = SingleAssignmentDisposable()
                 subscription.disposable = d
                 d.disposable = result.subscribe(observer)
-            
+
             d1.disposable = source.subscribe(
-                observer.on_next, 
-                on_error, 
+                observer.on_next,
+                on_error,
                 observer.on_completed
             )
             return subscription
         return AnonymousObservable(subscribe)
 
     def __catch_exception(self, second=None, handler=None):
-        """Continues an observable sequence that is terminated by an exception 
+        """Continues an observable sequence that is terminated by an exception
         with the next observable sequence.
-     
+
         1 - xs.catch_exception(ys)
         2 - xs.catch_exception(lambda ex: ys(ex))
-    
+
         Keyword arguments:
-        handler -- Exception handler function that returns an observable 
+        handler -- Exception handler function that returns an observable
             sequence  given the error that occurred in the first sequence.
-        second -- Second observable sequence used to produce results when an 
+        second -- Second observable sequence used to produce results when an
             error occurred in the first sequence.
-    
-        Returns an observable sequence containing the first sequence's 
-        elements, followed by the elements of the handler sequence in case an 
+
+        Returns an observable sequence containing the first sequence's
+        elements, followed by the elements of the handler sequence in case an
         exception occurred.
         """
-    
+
         if handler or not isinstance(second, Observable):
             return self.catch_handler(self, handler or second)
-        
+
         return Observable.catch_exception([self, second])
 
     @classmethod
     def catch_exception(cls, *args):
-        """Continues an observable sequence that is terminated by an 
+        """Continues an observable sequence that is terminated by an
         exception with the next observable sequence.
-     
+
         1 - res = Observable.catch_exception(xs, ys, zs)
         2 - res = Observable.catch_exception([xs, ys, zs])
-    
-        Returns an observable sequence containing elements from consecutive 
+
+        Returns an observable sequence containing elements from consecutive
         source sequences until a source sequence terminates successfully.
         """
 
@@ -84,7 +84,7 @@ class ObservableCatch(Observable):
         else:
             sources = list(args)
 
-        #return Enumerable.catch_exception(Enumerable.for_each(items))
+        #return Enumerable.catch_exception(Enumerable.for_each(sources))
 
         def subscribe(observer):
             e = iter(sources)
@@ -94,7 +94,7 @@ class ObservableCatch(Observable):
 
             def action(action1, state=None):
                 current = None
-                
+
                 def on_error(exn):
                     last_exception[0] = exn
                     action1()
@@ -107,13 +107,13 @@ class ObservableCatch(Observable):
                     if last_exception[0]:
                         observer.on_error(last_exception[0])
                     else:
-                        observer.on_completed()    
+                        observer.on_completed()
                 except Exception as ex:
                     observer.on_error(ex)
                 else:
                     d = SingleAssignmentDisposable()
                     subscription.disposable = d
-                    
+
                     d.disposable = current.subscribe(
                         observer.on_next,
                         on_error,
@@ -121,7 +121,7 @@ class ObservableCatch(Observable):
                     )
 
             cancelable = immediate_scheduler.schedule_recursive(action)
-            
+
             def dispose():
                 is_disposed[0] = True
             return CompositeDisposable(subscription, cancelable, Disposable(dispose))
