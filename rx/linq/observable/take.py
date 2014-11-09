823de05f2bc34befb31c82cf63cb46a170397bd2
Dag Brattli <dag@brattli.net>
2014-11-09 13:58:37 +0000

    Bugfix. Call lowercase method

--- rx/linq/observable/take.py
+++ rx/linq/observable/take.py
@@ -12,25 +12,25 @@ class ObservableTake(Observable):
         """Returns a specified number of contiguous elements from the start of
         an observable sequence, using the specified scheduler for the edge case
         of take(0).
-        
+
         1 - source.take(5)
         2 - source.take(0, rx.Scheduler.timeout)
-        
+
         Keyword arguments:
         count -- The number of elements to return.
-        scheduler -- [Optional] Scheduler used to produce an OnCompleted 
+        scheduler -- [Optional] Scheduler used to produce an OnCompleted
             message in case count is set to 0.
 
-        Returns an observable sequence that contains the specified number of 
+        Returns an observable sequence that contains the specified number of
         elements from the start of the input sequence.
         """
 
         if count < 0:
             raise ArgumentOutOfRangeException()
-        
+
         if not count:
-            return Observable.Empty(scheduler)
-        
+            return Observable.empty(scheduler)
+
         observable = self
         def subscribe(observer):
             remaining = [count]
@@ -41,6 +41,6 @@ class ObservableTake(Observable):
                     observer.on_next(value)
                     if not remaining[0]:
                         observer.on_completed()
-                    
+
             return observable.subscribe(on_next, observer.on_error, observer.on_completed)
         return AnonymousObservable(subscribe)
