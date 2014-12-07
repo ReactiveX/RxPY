3677f472139a5c15bfffbecdbcf12cf0f87c763d
Dag Brattli <dag@brattli.net>
2014-12-07 08:18:37 +0000

    Pairwise now emits a tuple instead of a list

--- tests/test_observable/test_pairwise.py
+++ tests/test_observable/test_pairwise.py
@@ -45,17 +45,17 @@ class TestPairwise(unittest.TestCase):
             on_next(210, 4),
             on_completed(220)
         )
-    
+
         def create():
             return xs.pairwise()
-    
+
         results = scheduler.start(create)
-        
-    
+
+
         results.messages.assert_equal(
             on_completed(220)
         )
-    
+
         xs.subscriptions.assert_equal(
             subscribe(200, 220)
         )
@@ -72,19 +72,19 @@ class TestPairwise(unittest.TestCase):
             on_next(350, 1),
             on_completed(360)
         )
-        
+
         def create():
             return xs.pairwise()
-        
+
         results = scheduler.start(create)
-        
+
         results.messages.assert_equal(
-            on_next(240, [4,3]),
-            on_next(290, [3, 2]),
-            on_next(350, [2, 1]),
+            on_next(240, (4,3)),
+            on_next(290, (3, 2)),
+            on_next(350, (2, 1)),
             on_completed(360)
         )
-        
+
         xs.subscriptions.assert_equal(
             subscribe(200, 360)
          )
@@ -105,12 +105,12 @@ class TestPairwise(unittest.TestCase):
             return xs.pairwise()
 
         results = scheduler.start(create)
-        
+
 
         results.messages.assert_equal(
-            on_next(240, [4,3]),
-            on_next(290, [3, 2]),
-            on_next(350, [2, 1])
+            on_next(240, (4,3)),
+            on_next(290, (3, 2)),
+            on_next(350, (2, 1))
         )
 
         xs.subscriptions.assert_equal(
@@ -121,7 +121,7 @@ class TestPairwise(unittest.TestCase):
     def test_pairwise_error(self):
         error = Exception()
         scheduler = TestScheduler()
-    
+
         xs = scheduler.create_hot_observable(
             on_next(180, 5),
             on_next(210, 4),
@@ -130,25 +130,25 @@ class TestPairwise(unittest.TestCase):
             on_next(350, 1),
             on_completed(360)
           )
-        
+
         def create():
             return xs.pairwise()
-    
+
         results = scheduler.start(create)
-        
-    
+
+
         results.messages.assert_equal(
-            on_next(240, [4,3]),
+            on_next(240, (4,3)),
             on_error(290, error)
         )
-    
+
         xs.subscriptions.assert_equal(
             subscribe(200, 290)
         )
-    
+
     def test_pairwise_disposed(self):
         scheduler = TestScheduler()
-    
+
         xs = scheduler.create_hot_observable(
             on_next(180, 5),
             on_next(210, 4),
@@ -157,16 +157,16 @@ class TestPairwise(unittest.TestCase):
             on_next(350, 1),
             on_completed(360)
           )
-    
+
         def create():
             return xs.pairwise()
-      
+
         results = scheduler.start(create, disposed=280)
-        
+
         results.messages.assert_equal(
-            on_next(240, [4,3])
+            on_next(240, (4,3))
         )
-    
+
         xs.subscriptions.assert_equal(
             subscribe(200, 280)
         )
