28369c619d8b76761b827318278a883941a56864
Dag Brattli <dag@brattli.net>
2014-12-07 08:21:42 +0000

    Cleanup

--- rx/linq/observable/takeuntil.py
+++ rx/linq/observable/takeuntil.py
@@ -11,21 +11,23 @@ class ObservableTakeUntil(Observable):
     """Uses a meta class to extend Observable with the methods in this class"""
 
     def take_until(self, other):
-        """Returns the values from the source observable sequence until the 
+        """Returns the values from the source observable sequence until the
         other observable sequence produces a value.
 
-        Keyword arguments:    
-        other -- Observable sequence that terminates propagation of elements of 
+        Keyword arguments:
+        other -- Observable sequence that terminates propagation of elements of
             the source sequence.
-    
-        Returns an observable sequence containing the elements of the source 
-        sequence up to the point the other sequence interrupted further propagation.
+
+        Returns an observable sequence containing the elements of the source
+        sequence up to the point the other sequence interrupted further
+        propagation.
         """
+
         source = self
         other = Observable.from_future(other)
 
         def subscribe(observer):
-            
+
             def on_completed(x):
                 observer.on_completed()
 
