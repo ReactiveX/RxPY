f14caaa83ccad8fbaed8671edfa7a003a6627314
Dag Brattli <dag@brattli.net>
2014-12-07 17:24:30 +0100

    RxPY meets Rx.NET

---
+++ examples/ironpython/meetdotnet.py
@@ -0,0 +1,34 @@
+import clr
+import sys
+import time
+
+# You need to adjuct these after your environment
+sys.path.append(r".") # Location of rx
+sys.path.append(r"Library/Frameworks/Mono.framework/Versions/3.10.0/lib/mono/gac/System.Reactive.Core/2.2.0.0__31bf3856ad364e35/")
+sys.path.append(r"/Library/Frameworks/Mono.framework/Versions/3.10.0/lib/mono/gac/System.Reactive.Linq/2.2.0.0__31bf3856ad364e35/")
+
+clr.AddReference("System.Reactive.Core")
+clr.AddReference("System.Reactive.Linq")
+
+# Import Rx.NET
+from System.Reactive.Linq import Observable
+from System.Reactive import Observer
+
+# Import RxPY
+import rx
+
+stream = rx.subjects.Subject()
+
+def on_next(x):
+	print "rxpy: %s" % x
+stream.subscribe(on_next)
+
+def OnNext(x):
+	print "rx.net: %s" % x
+	stream.on_next(x)
+obs = Observer.Create[int](OnNext)
+
+xs = Observable.Range(1, 10)
+xs.Subscribe(obs)
+
+print "That was a lot of fun!"
