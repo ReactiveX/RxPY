27e8e4a07726db656f50e5b0e21c30731aaf63e5
Dag Brattli <dag@brattli.net>
2014-12-21 14:43:06 +0000

    Docstring and comment changes

--- rx/linq/observable/blocking/foreach.py
+++ rx/linq/observable/blocking/foreach.py
@@ -4,7 +4,7 @@ from rx.blockingobservable import BlockingObservable
 from rx.internal import extensionmethod
 
 @extensionmethod(BlockingObservable)
-def for_each(self, on_next):
+def for_each(self, action):
     """Invokes a method on each item emitted by this BlockingObservable and
     blocks until the Observable completes.
 
@@ -17,9 +17,9 @@ def for_each(self, on_next):
     throw an exception.
 
     Keyword parameters:
-    :param types.FunctionType on_next: the action to invoke for each item
+    :param types.FunctionType action: the action to invoke for each item
         emitted by the `BlockingObservable`.
-    :raises RuntimeException: if an error occurs
+    :raises Exception: if an error occurs
     :returns: None
     :rtype: None
     """
@@ -27,18 +27,15 @@ def for_each(self, on_next):
     latch = threading.Event()
     exception = None
 
-    def _on_next(value):
-        on_next(value)
+    def on_next(value):
+        action(value)
 
     def on_error(err):
-        #
         # If we receive an onError event we set the reference on the
-        # outer thread so we can git it and throw after the
-        # latch.await().
+        # outer thread so we can git it and throw after the latch.wait()
         #
         # We do this instead of throwing directly since this may be on
         # a different thread and the latch is still waiting.
-        #
         exception = err
         latch.set()
 
