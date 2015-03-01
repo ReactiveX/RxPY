868fd0c70db9585976d360b086ed9a73a49aac78
38elements <mh19820223@gmail.com>
2015-03-01 17:27:32 +0900

    Add examples/parallel/timer.py

---
+++ examples/parallel/timer.py
@@ -0,0 +1,25 @@
+import rx
+import concurrent.futures
+import time
+
+seconds = [5, 1, 2, 4, 3]
+
+
+def sleep(t):
+    time.sleep(t)
+    return t
+
+
+def output(result):
+    print '%d seconds' % result
+
+with concurrent.futures.ProcessPoolExecutor(5) as executor:
+    rx.Observable.from_(seconds).flat_map(
+        lambda s: executor.submit(sleep, s)
+    ).subscribe(output)
+
+# 1 seconds
+# 2 seconds
+# 3 seconds
+# 4 seconds
+# 5 seconds
