3cabbca67684206e0f8bf82cba6b8f56d06280c9
Dag Brattli <dag@brattli.net>
2015-03-25 21:29:19 +0100

    Added alternative adapt_call if no inspect module

--- rx/internal/utils.py
+++ rx/internal/utils.py
@@ -1,10 +1,14 @@
-from inspect import getargspec
 import types
+try:
+    from inspect import getargspec
+except ImportError:
+    getargspec = None
 
 from rx import AnonymousObservable
 from rx.disposables import CompositeDisposable
 
 from .exceptions import DisposedException
+
 
 def add_ref(xs, r):
     def subscribe(observer):
@@ -12,7 +16,8 @@ def add_ref(xs, r):
 
     return AnonymousObservable(subscribe)
 
-def adapt_call(func):
+
+def adapt_call_1(func):
     """Adapts func from taking 3 params to only taking 1 or 2 params"""
 
     func_types = (
@@ -24,6 +29,7 @@ def adapt_call(func):
 
     def func1(arg1, *_):
         return func(arg1)
+
     def func2(arg1, arg2=None, *_):
         return func(arg1, arg2)
 
@@ -41,19 +47,52 @@ def adapt_call(func):
 
     return func_wrapped
 
+
+def adapt_call_2(func):
+    """Adapts func from taking n params to only taking 1 or 2 params"""
+
+    def func1(arg1, *_):
+        return func(arg1)
+
+    def func2(arg1, arg2=None, *_):
+        return func(arg1, arg2)
+
+    def func_wrapped(*args, **kw):
+        for fn in (func1, func2):
+            try:
+                return fn(*args, **kw)
+            except TypeError:
+                pass
+        # Error if we get here. Just call original function to generate a
+        # meaningful error message
+        return func(*args, **kw)
+    return func_wrapped
+
+# if we have getargspec we use adapt_call_1, since it's faster
+if getargspec is not None:
+    adapt_call = adapt_call_1
+else:
+    adapt_call = adapt_call_2
+
+
 def check_disposed(this):
     if this.is_disposed:
         raise DisposedException()
 
+
 def is_future(p):
     return callable(getattr(p, "add_done_callback", None))
 
+
 class TimeInterval(object):
+
     def __init__(self, value, interval):
         self.value = value
         self.interval = interval
 
+
 class Timestamp(object):
+
     def __init__(self, value, timestamp):
         self.value = value
         self.timestamp = timestamp
