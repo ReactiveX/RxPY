63a497d98f7f33204efc1bc65996a58fda38da7e
Dag Brattli <dag@brattli.net>
2015-11-08 11:37:06 +0100

    Reverted dispose logic since it's already handled by schedule_recursive(). At least it should be.

--- rx/linq/observable/fromiterable.py
+++ rx/linq/observable/fromiterable.py
@@ -1,7 +1,6 @@
 from rx.observable import Observable
 from rx.anonymousobservable import AnonymousObservable
 from rx.concurrency import current_thread_scheduler
-from rx.disposables import Disposable, CompositeDisposable
 from rx.internal import extensionclassmethod
 
 
@@ -27,11 +26,8 @@ def from_iterable(cls, iterable, scheduler=None):
 
     def subscribe(observer):
         iterator = iter(iterable)
-        is_disposed = [False]
 
         def action(action1, state=None):
-            if is_disposed[0]:
-                return
             try:
                 item = next(iterator)
             except StopIteration:
@@ -40,11 +36,5 @@ def from_iterable(cls, iterable, scheduler=None):
                 observer.on_next(item)
                 action1(action)
 
-        cancelable = scheduler.schedule_recursive(action)
-
-        def dispose():
-            is_disposed[0] = True
-
-        return CompositeDisposable(cancelable, Disposable(dispose))
-
+        return scheduler.schedule_recursive(action)
     return AnonymousObservable(subscribe)
