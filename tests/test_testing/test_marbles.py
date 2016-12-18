cce680070ca4daa675b5cf838b9911a93384c84d
Gunther Klessinger <gk@axiros.com>
2016-12-18 02:11:02 +0100

    evaluating "0" in marble strings (plus a first test for the testing module)

--- tests/test_testing/test_marbles.py
+++ tests/test_testing/test_marbles.py
@@ -5,7 +5,8 @@ def test_alias():
     assert rx.Observable.from_string == rx.Observable.from_marbles
 
 def test_from_to_marbles():
-    for marbles in '0-1-(10)-|', '0|', '(10)-(20)|', 'abc|':
+    for marbles in '0-1-(10)-|', '0|', '(10)-(20)|', '(abc)|':
+        #from nose.tools import set_trace; set_trace()
         stream = rx.Observable.from_string(marbles)
         assert stream.to_blocking().to_marbles() == marbles
 
