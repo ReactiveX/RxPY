adcac925723d12b2c8ab834d9299177f682b77a4
Gunther Klessinger <gk@axiros.com>
2016-12-19 12:03:10 +0100

    Fixing intermittent errors in the marbles test using the default scheduler. Added tests with test_scheduler

--- tests/test_testing/test_marbles.py
+++ tests/test_testing/test_marbles.py
@@ -1,14 +1,38 @@
 import rx
-from rx.testing import marbles
+import unittest
+from rx.testing import marbles, TestScheduler
 
-def test_alias():
-    assert rx.Observable.from_string == rx.Observable.from_marbles
+class TestMarbles(unittest.TestCase):
+    def test_alias(self):
+        assert rx.Observable.from_string == rx.Observable.from_marbles
 
-def test_from_to_marbles():
-    for marbles in '0-1-(10)-|', '0|', '(10)-(20)|', '(abc)|':
-        #from nose.tools import set_trace; set_trace()
-        stream = rx.Observable.from_string(marbles)
-        assert stream.to_blocking().to_marbles() == marbles
+    def test_from_to_marbles(self):
+        'using default scheduler'
+        # GK: in the marbles test strings, always add a tick ("-") at the end,
+        # when using the default scheduler.
+        # Otherwise you might get intermittent(!) failures (missing last events).
+        # Which breaks travis(!).
+        # If unsure let it run a while, e.g. on bash:
+        # `while true; do nosetests -w "tests/test_testing"  -sv || break; done`
+        for marbles in '0-1-(10)-|', '0-|', '(10)-(20)-|', '(abc)-|':
+            #from nose.tools import set_trace; set_trace()
+            stream = rx.Observable.from_string(marbles)
+            res = stream.to_blocking().to_marbles()
+            self.assertEqual(res, marbles)
+
+
+
+    def test_from_to_marbles_with_testscheduler(self):
+        'using test scheduler'
+        scheduler = TestScheduler()
+        for marbles in '0-1-(10)-|', '0-|', '(10)-(20)-|', '(abc)-|':
+            #from nose.tools import set_trace; set_trace()
+            stream = rx.Observable.from_string(marbles)
+            res = stream.to_blocking().to_marbles(scheduler=scheduler)
+            # the test scheduler uses virtual time => `to_marbles` does not
+            # see the original delays:
+            self.assertEqual(res, marbles.replace('-', ''))
+
 
 
 
