21254c3d215b9c8013f7ac7845835ce311c90dc3
Gunther Klessinger <gk@axiros.com>
2016-12-22 16:08:03 +0100

    asyncio import for py3

--- tests/test_observable/py3_tofuture.py
+++ tests/test_observable/py3_tofuture.py
@@ -5,7 +5,9 @@ from nose import SkipTest
 import sys
 if sys.version_info.major < 3:
     raise SkipTest("Py3 language async declaration required")
-
+asyncio = rx.config['asyncio']
+if asyncio is None:
+    raise SkipTest("asyncio not available")
 import rx
 from rx.core import Observable, Disposable
 from rx.testing import TestScheduler, ReactiveTest
