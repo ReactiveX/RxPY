597b116e7bf91bb3854826c143f9007bd450d550
Thomas Nield <thomasnield@live.com>
2017-02-22 22:53:29 -0600

    implement from_callable

--- rx/linq/observable/returnvalue.py
+++ rx/linq/observable/returnvalue.py
@@ -32,3 +32,34 @@ def return_value(cls, value, scheduler=None):
 
         return scheduler.schedule(action)
     return AnonymousObservable(subscribe)
+
+
+@extensionclassmethod(Observable)
+def from_callable(cls, supplier, scheduler=None):
+    """Returns an observable sequence that contains a single element generate from a supplier,
+       using the specified scheduler to send out observer messages.
+
+       example
+       res = rx.Observable.from_callable(lambda: calculate_value())
+       res = rx.Observable.from_callable(lambda: 1 / 0) # emits an error
+
+       Keyword arguments:
+       value -- Single element in the resulting observable sequence.
+       scheduler -- [Optional] Scheduler to send the single element on. If
+           not specified, defaults to Scheduler.immediate.
+
+       Returns an observable sequence containing the single specified
+       element derived from the supplier
+       """
+    scheduler = scheduler or current_thread_scheduler
+
+    def subscribe(observer):
+        def action(scheduler, state=None):
+            try:
+                observer.on_next(supplier())
+                observer.on_completed()
+            except Exception:
+                observer.on_error(Exception)
+        return scheduler.schedule(action)
+
+    return AnonymousObservable(subscribe)
