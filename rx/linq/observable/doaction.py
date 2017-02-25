1650a8c76a1c9983dd93983f0eaadc55a19cefb2
Thomas Nield <thomasnield@live.com>
2017-02-25 09:13:36 -0600

    implement do_on_subscribe(), do_on_dispose(), do_on_terminate(), do_after_terminate(), and do_finally()

--- rx/linq/observable/doaction.py
+++ rx/linq/observable/doaction.py
@@ -1,12 +1,13 @@
-from rx.core import Observer, Observable, AnonymousObservable
+from rx.core import Observer, Observable, AnonymousObservable, Disposable
 from rx.internal import extensionmethod
+from rx.disposables import CompositeDisposable
 
 
 @extensionmethod(Observable, alias="tap")
 def do_action(self, on_next=None, on_error=None, on_completed=None,
               observer=None):
     """Invokes an action for each element in the observable sequence and
-    invokes an action upon graceful or exceptional termination of the
+    invokes an action on graceful or exceptional termination of the
     observable sequence. This method can be used for debugging, logging,
     etc. of query behavior by intercepting the message stream to run
     arbitrary actions for messages on the pipeline.
@@ -19,9 +20,9 @@ def do_action(self, on_next=None, on_error=None, on_completed=None,
     observer -- [Optional] Observer, or ...
     on_next -- [Optional] Action to invoke for each element in the
         observable sequence.
-    on_error -- [Optional] Action to invoke upon exceptional termination
+    on_error -- [Optional] Action to invoke on exceptional termination
         of the observable sequence.
-    on_completed -- [Optional] Action to invoke upon graceful termination
+    on_completed -- [Optional] Action to invoke on graceful termination
         of the observable sequence.
 
     Returns the source sequence with the side-effecting behavior applied.
@@ -71,5 +72,178 @@ def do_action(self, on_next=None, on_error=None, on_completed=None,
                     observer.on_error(e)
 
                 observer.on_completed()
+
         return source.subscribe(_on_next, _on_error, _on_completed)
+
     return AnonymousObservable(subscribe)
+
+
+@extensionmethod(Observable)
+def do_after_next(self, after_next):
+    """Invokes an action with each element after it has been emitted downstream.
+    This can be helpful for debugging, logging, and other side effects.
+
+    after_next -- Action to invoke on each element after it has been emitted
+    """
+
+    def subscribe(observer):
+
+        def on_next(value):
+            try:
+                observer.on_next(value)
+                after_next(value)
+            except Exception as e:
+                observer.on_error(e)
+
+        return self.subscribe(on_next, observer.on_error, observer.on_completed)
+
+    return AnonymousObservable(subscribe)
+
+
+@extensionmethod(Observable)
+def do_on_subscribe(self, on_subscribe):
+    """Invokes an action on subscription.
+    This can be helpful for debugging, logging, and other side effects on the start of an operation.
+
+    on_subscribe -- Action to invoke on subscription
+    """
+    def subscribe(observer):
+        on_subscribe()
+        return self.subscribe(observer.on_next, observer.on_error, observer.on_completed)
+
+    return AnonymousObservable(subscribe)
+
+
+@extensionmethod(Observable)
+def do_on_dispose(self, on_dispose):
+    """Invokes an action on disposal.
+     This can be helpful for debugging, logging, and other side effects on the disposal of an operation.
+
+
+    on_dispose -- Action to invoke on disposal
+    """
+
+    class OnDispose(Disposable):
+        def dispose(self):
+            on_dispose()
+
+    def subscribe(observer):
+        composite_disposable = CompositeDisposable()
+        composite_disposable.add(OnDispose())
+        disposable = self.subscribe(observer.on_next, observer.on_error, observer.on_completed)
+        composite_disposable.add(disposable)
+        return composite_disposable
+
+    return AnonymousObservable(subscribe)
+
+
+@extensionmethod(Observable)
+def do_on_terminate(self, on_terminate):
+    """Invokes an action on an on_complete() or on_error() event.
+     This can be helpful for debugging, logging, and other side effects when completion or an error terminates an operation.
+
+
+    on_terminate -- Action to invoke when on_complete or on_error is called
+    """
+
+    def subscribe(observer):
+
+        def on_completed():
+            try:
+                on_terminate()
+            except Exception as err:
+                observer.on_error(err)
+            else:
+                observer.on_completed()
+
+        def on_error(exception):
+            try:
+                on_terminate()
+            except Exception as err:
+                observer.on_error(err)
+            else:
+                observer.on_error(exception)
+
+        return self.subscribe(observer.on_next, on_error, on_completed)
+
+    return AnonymousObservable(subscribe)
+
+
+@extensionmethod(Observable)
+def do_after_terminate(self, after_terminate):
+    """Invokes an action after an on_complete() or on_error() event.
+     This can be helpful for debugging, logging, and other side effects when completion or an error terminates an operation
+
+
+    on_terminate -- Action to invoke after on_complete or on_error is called
+    """
+    def subscribe(observer):
+
+        def on_completed():
+            observer.on_completed()
+            try:
+                after_terminate()
+            except Exception as err:
+                observer.on_error(err)
+
+        def on_error(exception):
+            observer.on_error(exception)
+            try:
+                after_terminate()
+            except Exception as err:
+                observer.on_error(err)
+
+        return self.subscribe(observer.on_next, on_error, on_completed)
+
+    return AnonymousObservable(subscribe)
+
+
+@extensionmethod(Observable)
+def do_finally(self, finally_action):
+    """Invokes an action after an on_complete(), on_error(), or disposal event occurs
+     This can be helpful for debugging, logging, and other side effects when completion, an error, or disposal terminates an operation.
+    Note this operator will strive to execute the finally_action once, and prevent any redudant calls
+
+    finally_action -- Action to invoke after on_complete, on_error, or disposal is called
+    """
+
+    class OnDispose(Disposable):
+        def __init__(self, was_invoked):
+            self.was_invoked = was_invoked
+
+        def dispose(self):
+            if not self.was_invoked[0]:
+                finally_action()
+                self.was_invoked[0] = True
+
+    def subscribe(observer):
+
+        was_invoked = [False]
+
+        def on_completed():
+            observer.on_completed()
+            try:
+                if not was_invoked[0]:
+                    finally_action()
+                    was_invoked[0] = True
+            except Exception as err:
+                observer.on_error(err)
+
+        def on_error(exception):
+            observer.on_error(exception)
+            try:
+                if not was_invoked[0]:
+                    finally_action()
+                    was_invoked[0] = True
+            except Exception as err:
+                observer.on_error(err)
+
+        composite_disposable = CompositeDisposable()
+        composite_disposable.add(OnDispose(was_invoked))
+        disposable = self.subscribe(observer.on_next, on_error, on_completed)
+        composite_disposable.add(disposable)
+
+        return composite_disposable
+
+    return AnonymousObservable(subscribe)
+
