a119a0c8e518869c17020fde49f96cd0ee8b04c0
Thomas Nield <thomasnield@live.com>
2017-02-27 19:49:31 -0600

    add variance() and standard_deviation()

--- rx/linq/observable/statistics.py
+++ rx/linq/observable/statistics.py
@@ -1,5 +1,6 @@
 from rx.core import Observable
 from rx.internal import extensionmethod
+import math
 
 
 def determine_median(sorted_list):
@@ -25,7 +26,8 @@ def median(self):
 @extensionmethod(Observable)
 def mode(self):
     """
-    returns the most frequently emitted value (or "values" if they have the same number of occurrences)
+    Returns the most frequently emitted value (or "values" if they have the same number of occurrences).
+    The sequence must be finite.
     """
     return self.group_by(lambda v: v) \
         .flat_map(lambda grp: grp.count().map(lambda ct: (grp.key, ct))) \
@@ -33,5 +35,28 @@ def mode(self):
         .flat_map(lambda l: Observable.from_(l).take_while(lambda t: t[1] == l[0][1])) \
         .map(lambda t: t[0])
 
+
+@extensionmethod(Observable)
+def variance(self):
+    """
+    Returns the statistical variance of the numerical emissions.
+    The sequence must be finite.
+    """
+    squared_values = self.to_list() \
+        .flat_map(lambda l: Observable.from_(l).average().flat_map(lambda avg: Observable.from_(l).map(lambda i: i - avg))) \
+        .map(lambda i: i * i) \
+        .publish() \
+        .auto_connect(2)
+
+    return Observable.zip(squared_values.sum(), squared_values.count(), lambda sum, ct: sum / (ct - 1))
+
+
+@extensionmethod(Observable)
+def standard_deviation(self):
+    """
+    Returns the standard deviation of the numerical emissions:
+    The sequence must be finite.
+    """
+    return self.variance().map(lambda i: math.sqrt(i))
 
 
