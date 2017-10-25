316414033d10d4ba8e550475babf1e049657d598
Neil Vyas <neil.vyas@affirm.com>
2017-10-24 17:16:37 -0700

    Coverage for error handling in adapt_call.

--- tests/test_core/test_util.py
+++ tests/test_core/test_util.py
@@ -48,3 +48,30 @@ class TestUtil(unittest.TestCase):
 
         value = func(42, 43, 44)
         assert value == 4200
+
+    def test_adapt_call_underlying_error(self):
+        err_msg = "We should see the original traceback."
+
+        def throws(a):
+            raise TypeError(err_msg)
+
+        with self.assertRaises(TypeError) as e:
+            adapt_call(throws)(None)
+
+        self.assertEqual(err_msg, e.exception.message)
+
+    def test_adapt_call_adaptation_error(self):
+
+        def not_adaptable(a, b, c):
+            pass
+
+        err_msg = "Couldn't adapt function {}".format(not_adaptable.__name__)
+
+        with self.assertRaises(TypeError) as e1:
+            adapt_call(not_adaptable)(None)
+
+        with self.assertRaises(TypeError) as e2:
+            adapt_call(not_adaptable)(None, None)
+
+        for e in (e1, e2):
+            self.assertEqual(err_msg, e.exception.message)
