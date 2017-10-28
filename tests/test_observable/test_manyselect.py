10a0683eec2aac07a6da7ce46f66ea3c0380a61b
Neil Vyas <neil.vyas@affirm.com>
2017-10-27 19:30:17 -0700

    Improving and fixing test coverage for law 1.

--- tests/test_observable/test_manyselect.py
+++ tests/test_observable/test_manyselect.py
@@ -3,6 +3,7 @@ import unittest
 from rx.core import Observable
 from rx.testing import TestScheduler, ReactiveTest
 
+
 on_next = ReactiveTest.on_next
 on_completed = ReactiveTest.on_completed
 on_error = ReactiveTest.on_error
@@ -12,15 +13,35 @@ disposed = ReactiveTest.disposed
 created = ReactiveTest.created
 
 
+def _extract(x):
+    return x.first()
+
+
+def _duplicate(x):
+    """This is ``pure`` or ``return``. We need it in order to compare equality
+    of inputs and outputs of ``many_select``, since we don't have a true
+    ``extract`` function.
+    """
+    return Observable.from_iterable([x]).first()
+
+
+def _comparer(x, y):
+    """Compares two items of type ``Observable[a]``."""
+    return x.sequence_equal(y).first()
+
+
 class TestManySelect(unittest.TestCase):
+
+    def _obs_equal(self, left, right):
+        return left.sequence_equal(right, _comparer).first().subscribe(self.assertTrue)
 
     def test_many_select_law_1(self):
         xs = Observable.range(1, 3)
 
-        left = xs.many_select(lambda x: x.first())
-        right = xs
+        left = xs.many_select(_extract)
+        right = xs.map(_duplicate)  # since .first() returns ``Observable[a]`` and not ``a``.
 
-        left.sequence_equal(right).first().subscribe(self.assertTrue)
+        self._obs_equal(left, right)
 
     def test_many_select_basic(self):
         scheduler = TestScheduler()
