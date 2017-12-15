4853b37229f3ad5bb93136d8a4a2aef859ca1d86
Dag Brattli <dag@brattli.net>
2017-12-15 12:26:04 +0100

    Refactored multicast

--- rx/core/observable.py
+++ rx/core/observable.py
@@ -369,6 +369,38 @@ class Observable(Generic[T_out], bases.Observable):
         source = self
         return map_indexed(mapper, source)
 
+
+    def multicast(self, subject=None, subject_selector=None, selector=None):
+        """Multicasts the source sequence notifications through an instantiated
+        subject into all uses of the sequence within a selector function. Each
+        subscription to the resulting sequence causes a separate multicast
+        invocation, exposing the sequence resulting from the selector function's
+        invocation. For specializations with fixed subject types, see Publish,
+        PublishLast, and Replay.
+
+        Example:
+        1 - res = source.multicast(observable)
+        2 - res = source.multicast(subject_selector=lambda scheduler: Subject(),
+                                selector=lambda x: x)
+
+        Keyword arguments:
+        subject_selector -- {Function} Factory function to create an
+            intermediate subject through which the source sequence's elements
+            will be multicast to the selector function.
+        subject -- Subject {Subject} to push source elements into.
+        selector -- {Function} [Optional] Optional selector function which can
+            use the multicasted source sequence subject to the policies enforced
+            by the created subject. Specified only if subject_selector" is a
+            factory function.
+
+        Returns an observable {Observable} sequence that contains the elements
+        of a sequence produced by multicasting the source sequence within a
+        selector function.
+        """
+        from ..operators.observable.multicast import multicast
+        source = self
+        return multicast(source, subject, subject_selector, selector)
+
     @classmethod
     def never(cls) -> 'Observable':
         """Returns a non-terminating observable sequence.
--- rx/operators/observable/multicast.py
+++ rx/operators/observable/multicast.py
@@ -1,11 +1,9 @@
 from rx import Observable, AnonymousObservable
 from rx.operators.connectableobservable import ConnectableObservable
 from rx.disposables import CompositeDisposable
-from rx.internal import extensionmethod
 
 
-@extensionmethod(Observable)
-def multicast(self, subject=None, subject_selector=None, selector=None):
+def multicast(source: Observable, subject=None, subject_selector=None, selector=None) -> Observable:
     """Multicasts the source sequence notifications through an instantiated
     subject into all uses of the sequence within a selector function. Each
     subscription to the resulting sequence causes a separate multicast
@@ -19,21 +17,19 @@ def multicast(self, subject=None, subject_selector=None, selector=None):
                                selector=lambda x: x)
 
     Keyword arguments:
-    subject_selector -- {Function} Factory function to create an
-        intermediate subject through which the source sequence's elements
-        will be multicast to the selector function.
-    subject -- Subject {Subject} to push source elements into.
-    selector -- {Function} [Optional] Optional selector function which can
-        use the multicasted source sequence subject to the policies enforced
+    subject_selector -- Factory function to create an intermediate
+        subject through which the source sequence's elements will be
+        multicast to the selector function.
+    subject -- Subject to push source elements into.
+    selector -- [Optional] Optional selector function which can use the
+        multicasted source sequence subject to the policies enforced
         by the created subject. Specified only if subject_selector" is a
         factory function.
 
-    Returns an observable {Observable} sequence that contains the elements
-    of a sequence produced by multicasting the source sequence within a
+    Returns an observable sequence that contains the elements of a
+    sequence produced by multicasting the source sequence within a
     selector function.
     """
-
-    source = self
 
     if subject_selector:
         def subscribe(observer, scheduler=None):
