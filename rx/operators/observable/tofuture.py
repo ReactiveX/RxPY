4c01b9faed4612067d372c9bf588d0eefd15f64e
Dag Brattli <dag@brattli.net>
2018-02-17 23:23:51 +0100

    Simplify

--- rx/operators/observable/tofuture.py
+++ rx/operators/observable/tofuture.py
@@ -8,11 +8,7 @@ def to_future(source: ObservableBase, future_ctor: Callable[[], Future] = None) 
     """Converts an existing observable sequence to a Future.
 
     Example:
-    future = rx.Observable.return_value(42).to_future(trollius.Future);
-
-    With config:
-    rx.config["Future"] = trollius.Future
-    future = rx.Observable.return_value(42).to_future()
+    future = rx.Observable.return_value(42).to_future(asyncio.Future);
 
     future_ctor -- {Functi[Optional] The constructor of the future.
         If not provided, it looks for it in rx.config.Future.
@@ -23,22 +19,19 @@ def to_future(source: ObservableBase, future_ctor: Callable[[], Future] = None) 
     future_ctor = future_ctor or Future
     future = future_ctor()
 
-    value = [None]
-    has_value = [False]
+    has_value = []
 
-    def on_next(v):
-        value[0] = v
-        has_value[0] = True
+    def on_next(value):
+        has_value.append(value)
 
     def on_error(err):
         future.set_exception(err)
 
     def on_completed():
-        if has_value[0]:
-            future.set_result(value[0])
+        if has_value:
+            future.set_result(has_value.pop())
 
     source.subscribe_(on_next, on_error, on_completed)
 
     # No cancellation can be done
     return future
-
