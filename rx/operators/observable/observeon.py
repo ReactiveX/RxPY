5a30f33ce549ef075f5fc12a8b853349cb406dbd
Dag Brattli <dag@brattli.net>
2018-06-23 08:07:19 +0200

    Bugfix. Don't overwrite the supplied scheduler

--- rx/operators/observable/observeon.py
+++ rx/operators/observable/observeon.py
@@ -3,21 +3,21 @@ from rx.core.observeonobserver import ObserveOnObserver
 
 
 def observe_on(source, scheduler) -> ObservableBase:
-    """Wraps the source sequence in order to run its observer callbacks on
-    the specified scheduler.
+    """Wraps the source sequence in order to run its observer callbacks
+    on the specified scheduler.
 
     Keyword arguments:
     scheduler -- Scheduler to notify observers on.
 
-    Returns the source sequence whose observations happen on the specified
-    scheduler.
+    Returns the source sequence whose observations happen on the
+    specified scheduler.
 
     This only invokes observer callbacks on a scheduler. In case the
     subscription and/or unsubscription actions have side-effects
     that require to be run on a scheduler, use subscribe_on.
     """
 
-    def subscribe(observer, scheduler=None):
+    def subscribe(observer, _=None):
         return source.subscribe(ObserveOnObserver(scheduler, observer))
 
     return AnonymousObservable(subscribe)
