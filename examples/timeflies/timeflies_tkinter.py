675df673865aeedfd38d3929505c1448aee22a86
Dag Brattli <dag@brattli.net>
2019-01-01 10:03:35 +0100

    Fixed timeflies tkinter example. Really cool!

--- examples/timeflies/timeflies_tkinter.py
+++ examples/timeflies/timeflies_tkinter.py
@@ -1,7 +1,12 @@
 from tkinter import Tk, Label, Frame
 
+from rx import Observable
 from rx.subjects import Subject
 from rx.concurrency import TkinterScheduler
+
+from rx.operators.delay import delay
+from rx.operators.map import map
+from rx.operators.flatmap import flat_mapi
 
 
 def main():
@@ -13,25 +18,35 @@ def main():
 
     frame = Frame(root, width=600, height=600)
 
-    def move(event):
-        mousemove.on_next(event)
-    frame.bind("<Motion>", move)
+    frame.bind("<Motion>", mousemove.on_next)
 
     text = 'TIME FLIES LIKE AN ARROW'
     labels = [Label(frame, text=c) for c in text]
 
-    def handle_label(i, label):
+    def on_next(info):
+        label, ev, i = info
+        label.place(x=ev.x + i*12 + 15, y=ev.y)
+
+    def handle_label(label, i):
         label.config(dict(borderwidth=0, padx=0, pady=0))
 
-        def on_next(ev):
-            label.place(x=ev.x + i*12 + 15, y=ev.y)
-        mousemove.delay(i*100).subscribe(on_next, scheduler=scheduler)
+        mapper = map(lambda ev: (label, ev, i))
+        delayer = delay(i*100)
 
-    for i, label in enumerate(labels):
-        handle_label(i, label)
+        return mousemove.pipe(
+            delayer,
+            mapper
+        )
+
+    labler = flat_mapi(handle_label)
+
+    Observable.from_(labels).pipe(
+        labler
+    ).subscribe_(on_next, scheduler=scheduler)
 
     frame.pack()
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
