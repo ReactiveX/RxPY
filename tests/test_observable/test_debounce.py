4b2785f60709ad13196a624d0d2fab8c0bf56092
Dag Brattli <dag@brattli.net>
2019-01-04 08:58:31 +0100

    Fixed tests for create and debounce

--- tests/test_observable/test_debounce.py
+++ tests/test_observable/test_debounce.py
@@ -1,6 +1,6 @@
 import unittest
 
-from rx.chained import Observable
+from rx import empty, never, throw, operators as _
 from rx.testing import TestScheduler, ReactiveTest
 
 on_next = ReactiveTest.on_next
@@ -28,7 +28,7 @@ class TestDebounce(unittest.TestCase):
             300, 4), on_next(350, 5), on_next(400, 6), on_next(450, 7), on_next(500, 8), on_completed(550))
 
         def create():
-            return xs.debounce(40)
+            return xs.pipe(_.debounce(40))
 
         results = scheduler.start(create)
 
@@ -42,7 +42,7 @@ class TestDebounce(unittest.TestCase):
             300, 4), on_next(350, 5), on_next(400, 6), on_next(450, 7), on_next(500, 8), on_error(550, ex))
 
         def create():
-            return xs.debounce(40)
+            return xs.pipe(_.debounce(40))
 
         results = scheduler.start(create)
         assert results.messages == [on_next(290, 3), on_next(340, 4), on_next(
@@ -54,7 +54,7 @@ class TestDebounce(unittest.TestCase):
             300, 4), on_next(350, 5), on_next(400, 6), on_next(450, 7), on_next(500, 8), on_completed(550))
 
         def create():
-            return xs.debounce(60)
+            return xs.pipe(_.debounce(60))
 
         results = scheduler.start(create)
         assert results.messages == [on_next(550, 8), on_completed(550)]
@@ -66,7 +66,7 @@ class TestDebounce(unittest.TestCase):
             300, 4), on_next(350, 5), on_next(400, 6), on_next(450, 7), on_next(500, 8), on_error(550, ex))
 
         def create():
-            return xs.debounce(60)
+            return xs.pipe(_.debounce(60))
 
         results = scheduler.start(create)
         assert results.messages == [on_error(550, ex)]
@@ -77,7 +77,7 @@ class TestDebounce(unittest.TestCase):
             370, 4), on_next(421, 5), on_next(480, 6), on_next(490, 7), on_next(500, 8), on_completed(600))
 
         def create():
-            return xs.debounce(50)
+            return xs.pipe(_.debounce(50))
 
         results = scheduler.start(create)
         assert results.messages == [on_next(300, 2), on_next(
@@ -87,7 +87,7 @@ class TestDebounce(unittest.TestCase):
         scheduler = TestScheduler()
 
         def create():
-            return Observable.empty().debounce(10)
+            return empty().pipe(_.debounce(10))
 
         results = scheduler.start(create)
 
@@ -98,7 +98,7 @@ class TestDebounce(unittest.TestCase):
         scheduler = TestScheduler()
 
         def create():
-            return Observable.throw(ex).debounce(10)
+            return throw(ex).pipe(_.debounce(10))
 
         results = scheduler.start(create)
         assert results.messages == [on_error(200, ex)]
@@ -107,15 +107,21 @@ class TestDebounce(unittest.TestCase):
         scheduler = TestScheduler()
 
         def create():
-            return Observable.never().debounce(10)
+            return never().pipe(_.debounce(10))
 
         results = scheduler.start(create)
         assert results.messages == []
 
     def test_debounce_duration_delay_behavior(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(150, -1), on_next(250, 0), on_next(280, 1),
-                                             on_next(310, 2), on_next(350, 3), on_next(400, 4), on_completed(550))
+        xs = scheduler.create_hot_observable(
+            on_next(150, -1),
+            on_next(250, 0),
+            on_next(280, 1),
+            on_next(310, 2),
+            on_next(350, 3),
+            on_next(400, 4),
+            on_completed(550))
         ys = [scheduler.create_cold_observable(on_next(20, 42), on_next(25, 99)), scheduler.create_cold_observable(on_next(20, 42), on_next(25, 99)), scheduler.create_cold_observable(
             on_next(20, 42), on_next(25, 99)), scheduler.create_cold_observable(on_next(20, 42), on_next(25, 99)), scheduler.create_cold_observable(on_next(20, 42), on_next(25, 99))]
 
@@ -123,7 +129,7 @@ class TestDebounce(unittest.TestCase):
             def mapper(x):
                 return ys[x]
 
-            return xs.throttle_with_mapper(mapper)
+            return xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
@@ -146,7 +152,7 @@ class TestDebounce(unittest.TestCase):
         def create():
             def mapper(x):
                 return ys[x]
-            return xs.throttle_with_mapper(mapper)
+            return xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
@@ -160,15 +166,36 @@ class TestDebounce(unittest.TestCase):
 
     def test_debounce_duration_early_completion(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(150, -1), on_next(250, 0), on_next(280, 1),
-                                             on_next(310, 2), on_next(350, 3), on_next(400, 4), on_completed(410))
-        ys = [scheduler.create_cold_observable(on_next(20, 42), on_next(25, 99)), scheduler.create_cold_observable(on_next(40, 42), on_next(45, 99)), scheduler.create_cold_observable(
-            on_next(20, 42), on_next(25, 99)), scheduler.create_cold_observable(on_next(60, 42), on_next(65, 99)), scheduler.create_cold_observable(on_next(20, 42), on_next(25, 99))]
+        xs = scheduler.create_hot_observable(
+            on_next(150, -1),
+            on_next(250, 0),
+            on_next(280, 1),
+            on_next(310, 2),
+            on_next(350, 3),
+            on_next(400, 4),
+            on_completed(410))
+        ys = [
+            scheduler.create_cold_observable(
+                on_next(20, 42),
+                on_next(25, 99)),
+            scheduler.create_cold_observable(
+                on_next(40, 42),
+                on_next(45, 99)),
+            scheduler.create_cold_observable(
+                on_next(20, 42),
+                on_next(25, 99)),
+            scheduler.create_cold_observable(
+                on_next(60, 42),
+                on_next(65, 99)),
+            scheduler.create_cold_observable(
+                on_next(20, 42),
+                on_next(25, 99))
+        ]
 
         def create():
             def mapper(x):
                 return ys[x]
-            return xs.throttle_with_mapper(mapper)
+            return xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
@@ -193,7 +220,7 @@ class TestDebounce(unittest.TestCase):
                 else:
                     return scheduler.create_cold_observable(on_error(x * 10, ex))
 
-            return xs.throttle_with_mapper(mapper)
+            return xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
@@ -209,7 +236,7 @@ class TestDebounce(unittest.TestCase):
         def create():
             def mapper(x):
                 return scheduler.create_cold_observable(on_next(x * 10, "Ignore"), on_next(x * 10 + 5, "Aargh!"))
-            return xs.throttle_with_mapper(mapper)
+            return  xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
@@ -229,7 +256,7 @@ class TestDebounce(unittest.TestCase):
                 else:
                     _raise(ex)
 
-            return xs.throttle_with_mapper(mapper)
+            return  xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
@@ -244,23 +271,32 @@ class TestDebounce(unittest.TestCase):
         def create():
             def mapper(x):
                 return scheduler.create_cold_observable(on_completed(x * 10))
-            return xs.throttle_with_mapper(mapper)
+            return  xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
-        assert results.messages == [on_next(250 + 2 * 10, 2), on_next(350 + 3 * 10, 3),
-                                    on_next(450 + 4 * 10, 4), on_completed(550)]
+        assert results.messages == [
+            on_next(250 + 2 * 10, 2),
+            on_next(350 + 3 * 10, 3),
+            on_next(450 + 4 * 10, 4),
+            on_completed(550)]
         assert xs.subscriptions == [subscribe(200, 550)]
 
     def test_debounce_duration_inner_done_throttle_behavior(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(150, 1), on_next(250, 2), on_next(
-            280, 3), on_next(300, 4), on_next(400, 5), on_next(410, 6), on_completed(550))
+        xs = scheduler.create_hot_observable(
+            on_next(150, 1),
+            on_next(250, 2),
+            on_next(280, 3),
+            on_next(300, 4),
+            on_next(400, 5),
+            on_next(410, 6),
+            on_completed(550))
 
         def create():
             def mapper(x):
                 return scheduler.create_cold_observable(on_completed(x * 10))
-            return xs.throttle_with_mapper(mapper)
+            return  xs.pipe(_.throttle_with_mapper(mapper))
 
         results = scheduler.start(create)
 
