5f4e37dae15f8d889eec1ae74ff5c6b51a939e22
Dag Brattli <dag@brattli.net>
2019-01-05 13:41:29 +0100

    Fixed timestamp and throttle_with_mapper.

--- rx/core/operators/timestamp.py
+++ rx/core/operators/timestamp.py
@@ -7,25 +7,27 @@ from rx.internal.utils import Timestamp
 from rx import operators
 
 
-def timestamp() -> Callable[[Observable], Observable]:
-    """Records the timestamp for each value in an observable sequence.
+def _timestamp() -> Callable[[Observable], Observable]:
+    def timestamp(source: Observable) -> Observable:
+        """Records the timestamp for each value in an observable sequence.
 
-    Examples:
-        >>> timestamp()
+        Examples:
+            >>> timestamp(source)
 
-    Produces objects with attributes `value` and `timestamp`, where
-    value is the original value.
+        Produces objects with attributes `value` and `timestamp`, where
+        value is the original value.
 
-    Returns:
-        An operator function that takes an observable source and returns
-        an observable sequence with timestamp information on values.
+        Args:
+            source: Observable source to timestamp.
+
+        Returns:
+            An observable sequence with timestamp information on values.
     """
 
-    def partial(source: Observable) -> Observable:
         def factory(scheduler=None):
             scheduler = scheduler or timeout_scheduler
             mapper = operators.map(lambda value: Timestamp(value=value, timestamp=scheduler.now))
 
             return source.pipe(mapper)
         return defer(factory)
-    return partial
+    return timestamp
