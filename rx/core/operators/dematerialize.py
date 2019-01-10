16d9ccfd918c646bfba2633abc664516813422f3
Dag Brattli <dag@brattli.net>
2019-01-10 12:24:52 +0100

    Refactored dematerialize

--- rx/core/operators/dematerialize.py
+++ rx/core/operators/dematerialize.py
@@ -1,19 +1,21 @@
-from rx.core import ObservableBase, AnonymousObservable
+from typing import Callable
+from rx.core import Observable, AnonymousObservable
 
 
-def dematerialize(self) -> ObservableBase:
-    """Dematerializes the explicit notification values of an observable
-    sequence as implicit notifications.
+def _dematerialize() -> Callable[[Observable], Observable]:
+    def dematerialize(source: Observable) -> Observable:
+        """Dematerializes the explicit notification values of an
+        observable sequence as implicit notifications.
 
-    Returns an observable sequence exhibiting the behavior corresponding to
-    the source sequence's notification values.
-    """
+        Returns:
+            An observable sequence exhibiting the behavior
+            corresponding to the source sequence's notification values.
+        """
 
-    source = self
+        def subscribe(observer, scheduler=None):
+            def on_next(value):
+                return value.accept(observer)
 
-    def subscribe(observer, scheduler=None):
-        def on_next(value):
-            return value.accept(observer)
-
-        return source.subscribe_(on_next, observer.on_error, observer.on_completed, scheduler)
-    return AnonymousObservable(subscribe)
+            return source.subscribe_(on_next, observer.on_error, observer.on_completed, scheduler)
+        return AnonymousObservable(subscribe)
+    return dematerialize
