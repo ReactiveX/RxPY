459953605a4df48c004e8bc93745f5de8087b2e0
Dag Brattli <dag@brattli.net>
2019-01-11 09:51:19 +0100

    Refactored last, materialize, max, min, minby, switchlatest

--- rx/core/operators/materialize.py
+++ rx/core/operators/materialize.py
@@ -3,17 +3,21 @@ from rx.core import Observable, AnonymousObservable
 from rx.core.notification import OnNext, OnError, OnCompleted
 
 
-def materialize() -> Callable[[Observable], Observable]:
-    """Materializes the implicit notifications of an observable sequence
-    as explicit notification values.
+def _materialize() -> Callable[[Observable], Observable]:
+    def materialize(source: Observable) -> Observable:
+        """Partially applied materialize operator.
 
-    Returns:
-        An operator function that takes an observable source and returns
-        an observable sequence containing the materialized notification
-        values from the source sequence.
-    """
+        Materializes the implicit notifications of an observable
+        sequence as explicit notification values.
 
-    def partial(source: Observable) -> Observable:
+        Args:
+            source: Source observable to materialize.
+
+        Returns:
+            An observable sequence containing the materialized
+            notification values from the source sequence.
+        """
+
         def subscribe(observer, scheduler=None):
             def on_next(value):
                 observer.on_next(OnNext(value))
@@ -28,4 +32,4 @@ def materialize() -> Callable[[Observable], Observable]:
 
             return source.subscribe_(on_next, on_error, on_completed, scheduler)
         return AnonymousObservable(subscribe)
-    return partial
+    return materialize
