80f8df60aaa24f87a36b63134fb86ab73e0d1134
Dag Brattli <dag@brattli.net>
2019-01-12 09:32:00 +0100

    Fixes for combine_latest

---
+++ rx/core/observable/combinelatest.py
@@ -0,0 +1,80 @@
+from typing import Any, Callable, Iterable, Union, List, cast
+
+from rx.core import Observable, AnonymousObservable, typing
+from rx.disposables import CompositeDisposable, SingleAssignmentDisposable
+
+
+def _combine_latest(*args: Union[Observable, Iterable[Observable]],
+                    mapper: Callable[[Any], Any]) -> Observable:
+    """Merges the specified observable sequences into one observable
+    sequence by using the mapper function whenever any of the
+    observable sequences produces an element.
+
+    Examples:
+        >>> obs = combine_latest(obs1, obs2, obs3, lambda o1, o2, o3: o1 + o2 + o3)
+        >>> obs = combine_latest([obs1, obs2, obs3], lambda o1, o2, o3: o1 + o2 + o3)
+
+    Returns:
+        An observable sequence containing the result of combining
+        elements of the sources using the specified result mapper
+        function.
+    """
+
+    sources: List[Observable] = []
+
+    if isinstance(args[0], Iterable):
+        sources += list(args[0])
+    else:
+        sources += list(cast(Iterable[Observable], args))
+
+    result_mapper = mapper
+    parent = sources[0]
+
+    def subscribe(observer: typing.Observer, scheduler: typing.Scheduler = None):
+        n = len(sources)
+        has_value = [False] * n
+        has_value_all = [False]
+        is_done = [False] * n
+        values = [None] * n
+
+        def _next(i):
+            has_value[i] = True
+
+            if has_value_all[0] or all(has_value):
+                try:
+                    res = result_mapper(*values)
+                except Exception as ex:
+                    observer.on_error(ex)
+                    return
+
+                observer.on_next(res)
+            elif all([x for j, x in enumerate(is_done) if j != i]):
+                observer.on_completed()
+
+            has_value_all[0] = all(has_value)
+
+        def done(i):
+            is_done[i] = True
+            if all(is_done):
+                observer.on_completed()
+
+        subscriptions = [None] * n
+
+        def func(i):
+            subscriptions[i] = SingleAssignmentDisposable()
+
+            def on_next(x):
+                with parent.lock:
+                    values[i] = x
+                    _next(i)
+
+            def on_completed():
+                with parent.lock:
+                    done(i)
+
+            subscriptions[i].disposable = sources[i].subscribe_(on_next, observer.on_error, on_completed, scheduler)
+
+        for idx in range(n):
+            func(idx)
+        return CompositeDisposable(subscriptions)
+    return AnonymousObservable(subscribe)
--- rx/core/operators/combinelatest.py
+++ rx/core/operators/combinelatest.py
@@ -1,76 +1,30 @@
-from typing import Any, Callable, Iterable, Union
-from rx.core import ObservableBase, AnonymousObservable, typing
-from rx.disposables import CompositeDisposable, SingleAssignmentDisposable
+from typing import Any, Callable, Iterable, Union, List
+
+import rx
+from rx.core import Observable, typing
 
 
-def combine_latest(observables: Union[ObservableBase, Iterable[ObservableBase]],
-                   mapper: Callable[[Any], Any]) -> ObservableBase:
-    """Merges the specified observable sequences into one observable
-    sequence by using the mapper function whenever any of the
-    observable sequences produces an element.
+def _combine_latest(other: Union[Observable, Iterable[Observable]],
+                    mapper: Callable[[Any], Any]) -> Callable[[Observable], Observable]:
+    def combine_latest(source: Observable) -> Observable:
+        """Merges the specified observable sequences into one
+        observable sequence by using the mapper function whenever any
+        of the observable sequences produces an element.
 
-    1 - obs = Observable.combine_latest(obs1, obs2, obs3,
-                                       lambda o1, o2, o3: o1 + o2 + o3)
-    2 - obs = Observable.combine_latest([obs1, obs2, obs3],
-                                        lambda o1, o2, o3: o1 + o2 + o3)
+        Examples:
+            >>> obs = combine_latest(source)
 
-    Returns an observable sequence containing the result of combining
-    elements of the sources using the specified result mapper
-    function.
-    """
-    if isinstance(observables, typing.Observable):
-        observables = [observables]
+        Returns:
+            An observable sequence containing the result of combining
+            elements of the sources using the specified result mapper
+            function.
+        """
+        sources: List[Observable] = [source]
 
-    args = list(observables)
-    result_mapper = mapper
-    parent = args[0]
+        if isinstance(other, typing.Observable):
+            sources += [other]
+        else:
+            sources += other
 
-    def subscribe(observer, scheduler=None):
-        n = len(args)
-        has_value = [False] * n
-        has_value_all = [False]
-        is_done = [False] * n
-        values = [None] * n
-
-        def next(i):
-            has_value[i] = True
-
-            if has_value_all[0] or all(has_value):
-                try:
-                    res = result_mapper(*values)
-                except Exception as ex:
-                    observer.on_error(ex)
-                    return
-
-                observer.on_next(res)
-            elif all([x for j, x in enumerate(is_done) if j != i]):
-                observer.on_completed()
-
-            has_value_all[0] = all(has_value)
-
-        def done(i):
-            is_done[i] = True
-            if all(is_done):
-                observer.on_completed()
-
-        subscriptions = [None] * n
-
-        def func(i):
-            subscriptions[i] = SingleAssignmentDisposable()
-
-            def on_next(x):
-                with parent.lock:
-                    values[i] = x
-                    next(i)
-
-            def on_completed():
-                with parent.lock:
-                    done(i)
-
-            subscriptions[i].disposable = args[i].subscribe_(on_next, observer.on_error,
-                                                                      on_completed, scheduler)
-
-        for idx in range(n):
-            func(idx)
-        return CompositeDisposable(subscriptions)
-    return AnonymousObservable(subscribe)
+        return rx.combine_latest(sources, mapper)
+    return combine_latest
