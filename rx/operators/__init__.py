a975d307287c397654251fbdabdb6710a9ee2e8d
Dag Brattli <dag@brattli.net>
2019-01-14 22:58:17 +0100

    Fixed replay

--- rx/operators/__init__.py
+++ rx/operators/__init__.py
@@ -883,6 +883,42 @@ def reduce(accumulator: Callable[[Any, Any], Any], seed: Any = None) -> Callable
     return _reduce(accumulator, seed)
 
 
+def replay(mapper: Mapper = None, buffer_size: int = None, window: timedelta = None, scheduler: typing.Scheduler = None
+          ) -> Callable[[Observable], Union[Observable, ConnectableObservable]]:
+    """Returns an observable sequence that is the result of invoking the
+    mapper on a connectable observable sequence that shares a single
+    subscription to the underlying sequence replaying notifications
+    subject to a maximum time length for the replay buffer.
+
+    This operator is a specialization of Multicast using a
+    ReplaySubject.
+
+    Examples:
+        >>> res = replay(buffer_size=3)
+        >>> res = replay(buffer_size=3, window=500)
+        >>> res = replay(None, 3, 500)
+        >>> res = replay(lambda x: x.take(6).repeat(), 3, 500)
+
+    Args:
+        mapper: [Optional] Selector function which can use the multicasted
+            source sequence as many times as needed, without causing
+            multiple subscriptions to the source sequence. Subscribers to
+            the given source will receive all the notifications of the
+            source subject to the specified replay buffer trimming policy.
+        buffer_size: [Optional] Maximum element count of the replay
+            buffer.
+        window: [Optional] Maximum time length of the replay buffer.
+
+    Returns:
+        An operator function that takes an observable source and
+        returns an observable sequence that contains the elements of a
+        sequence produced by multicasting the source sequence within a
+        mapper function.
+    """
+    from rx.core.operators.replay import _replay
+    return _replay(mapper, buffer_size, window, scheduler)
+
+
 def scan(accumulator: Callable[[Any, Any], Any], seed: Any = None) -> Callable[[Observable], Observable]:
     """The scan operator.
 
