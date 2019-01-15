a1c0173b19617633dc42ed1dfd6962a5b25fd53f
Dag Brattli <dag@brattli.net>
2019-01-15 20:14:38 +0100

    Fixed pluck and pluck_attr

--- rx/core/operators/pluck.py
+++ rx/core/operators/pluck.py
@@ -1,35 +1,35 @@
-from typing import Any
-from rx.core import ObservableBase
+from typing import Any, Callable
+
+from rx import operators as ops
+from rx.core import Observable
 
 
-def pluck(source: ObservableBase, key: Any) -> ObservableBase:
+def _pluck(key: Any) -> Callable[[Observable], Observable]:
     """Retrieves the value of a specified key using dict-like access (as in
     element[key]) from all elements in the Observable sequence.
 
-    Keyword arguments:
-    key -- The key to pluck.
+    Args:
+        key: The key to pluck.
 
     Returns a new Observable {Observable} sequence of key values.
 
     To pluck an attribute of each element, use pluck_attr.
-
     """
 
-    return source.map(lambda x: x[key])
+    return ops.map(lambda x: x[key])
 
 
-def pluck_attr(source: ObservableBase, prop: str) -> ObservableBase:
+def _pluck_attr(prop: str) -> Callable[[Observable], Observable]:
     """Retrieves the value of a specified property (using getattr) from
     all elements in the Observable sequence.
 
-    Keyword arguments:
-    property -- The property to pluck.
+    Args:
+        property: The property to pluck.
 
     Returns a new Observable {Observable} sequence of property values.
 
     To pluck values using dict-like access (as in element[key]) on each
     element, use pluck.
-
     """
 
-    return source.map(lambda x: getattr(x, prop))
+    return ops.map(lambda x: getattr(x, prop))
