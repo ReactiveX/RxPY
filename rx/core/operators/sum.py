a6680274bbb1c9167ff990feebf39aa0b15e93f2
Dag Brattli <dag@brattli.net>
2019-01-15 15:06:26 +0100

    Fix error in sum operator

--- rx/core/operators/sum.py
+++ rx/core/operators/sum.py
@@ -6,12 +6,10 @@ from rx.core.typing import Mapper
 
 
 def _sum(key_mapper: Mapper = None) -> Callable[[Observable], Observable]:
-
     if key_mapper:
-        pipe(
+        return pipe(
             ops.map(key_mapper),
             ops.sum()
         )
 
-    reducing = ops.reduce(seed=0, accumulator=lambda prev, curr: prev + curr)
-    return pipe(reducing)
+    return ops.reduce(seed=0, accumulator=lambda prev, curr: prev + curr)
