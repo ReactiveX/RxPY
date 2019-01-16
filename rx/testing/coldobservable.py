ce533149ef372907e0fcb0460264a4d1983d15a9
Dag Brattli <dag@brattli.net>
2019-01-16 13:23:48 +0100

    Simplify code

--- rx/testing/coldobservable.py
+++ rx/testing/coldobservable.py
@@ -13,9 +13,6 @@ class ColdObservable(Observable):
         self.subscriptions: List[Subscription] = []
 
     def subscribe(self, observer=None, scheduler=None):
-        return self._subscribe_core(observer, scheduler)
-
-    def _subscribe_core(self, observer: typing.Observer, scheduler: typing.Scheduler = None) -> typing.Disposable:
         clock = self.scheduler.to_relative(self.scheduler.now)
         self.subscriptions.append(Subscription(clock))
         index = len(self.subscriptions) - 1
@@ -37,9 +34,6 @@ class ColdObservable(Observable):
         def dispose() -> None:
             start = self.subscriptions[index].subscribe
             end = self.scheduler.to_relative(self.scheduler.now)
-
-            print("Dispose cold at: %s" % self.scheduler.to_relative(self.scheduler.now))
-
             self.subscriptions[index] = Subscription(start, end)
             disposable.dispose()
 
