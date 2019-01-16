5c054d5479ac8f9e63f9a8e4b8f43158673eb3aa
Dag Brattli <dag@brattli.net>
2019-01-16 09:32:15 +0100

    Fixed observeon, subscribeon, toset and whiledo

--- rx/core/operators/observeon.py
+++ rx/core/operators/observeon.py
@@ -1,23 +1,29 @@
-from rx.core import AnonymousObservable, ObservableBase
+from typing import Callable
+
+from rx.core import AnonymousObservable, Observable
 from rx.core.observeonobserver import ObserveOnObserver
 
 
-def observe_on(source, scheduler) -> ObservableBase:
-    """Wraps the source sequence in order to run its observer callbacks
-    on the specified scheduler.
+def _observe_on(scheduler) -> Callable[[Observable], Observable]:
+    def observe_on(source: Observable) -> Observable:
+        """Wraps the source sequence in order to run its observer
+        callbacks on the specified scheduler.
 
-    Keyword arguments:
-    scheduler -- Scheduler to notify observers on.
+        This only invokes observer callbacks on a scheduler. In case
+        the subscription and/or unsubscription actions have
+        side-effects that require to be run on a scheduler, use
+        subscribe_on.
 
-    Returns the source sequence whose observations happen on the
-    specified scheduler.
+        Args:
+            source: Source observable.
 
-    This only invokes observer callbacks on a scheduler. In case the
-    subscription and/or unsubscription actions have side-effects
-    that require to be run on a scheduler, use subscribe_on.
-    """
 
-    def subscribe(observer, _=None):
-        return source.subscribe(ObserveOnObserver(scheduler, observer))
+        Returns:
+            Returns the source sequence whose observations happen on
+            the specified scheduler.
+        """
+        def subscribe(observer, _=None):
+            return source.subscribe(ObserveOnObserver(scheduler, observer))
 
-    return AnonymousObservable(subscribe)
+        return AnonymousObservable(subscribe)
+    return observe_on
