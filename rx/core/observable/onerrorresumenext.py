3986bd5163e6e7fa4524930c48548d2c0af19ae1
Dag Brattli <dag@brattli.net>
2019-01-16 18:37:51 +0100

    Fixed on_error_resume_next

---
+++ rx/core/observable/onerrorresumenext.py
@@ -0,0 +1,51 @@
+from rx.core import Observable, AnonymousObservable
+from rx.disposables import CompositeDisposable, SingleAssignmentDisposable, SerialDisposable
+from rx.internal.utils import is_future
+
+
+def _on_error_resume_next(*args) -> Observable:
+    """Continues an observable sequence that is terminated normally or
+    by an exception with the next observable sequence.
+
+    Examples:
+        >>> res = rx.on_error_resume_next(xs, ys, zs)
+        >>> res = rx.on_error_resume_next([xs, ys, zs])
+
+    Returns:
+        An observable sequence that concatenates the source sequences,
+        even if a sequence terminates exceptionally.
+    """
+    # curently not in:
+    # 3 - res = Observable.on_error_resume_next(xs, factory)
+
+    if args and isinstance(args[0], list):
+        sources = iter(args[0])
+    else:
+        sources = iter(args)
+
+    def subscribe(observer, scheduler=None):
+        subscription = SerialDisposable()
+        cancelable = SerialDisposable()
+
+        def action(scheduler, state=None):
+            try:
+                source = next(sources)
+            except StopIteration:
+                observer.on_completed()
+                return
+
+            # Allow source to be a factory method taking an error
+            source = source(state) if callable(source) else source
+            current = Observable.from_future(source) if is_future(source) else source
+
+            d = SingleAssignmentDisposable()
+            subscription.disposable = d
+
+            def on_resume(state=None):
+                scheduler.schedule(action, state)
+
+            d.disposable = current.subscribe_(observer.on_next, on_resume, on_resume, scheduler)
+
+        cancelable.disposable = scheduler.schedule(action)
+        return CompositeDisposable(subscription, cancelable)
+    return AnonymousObservable(subscribe)
--- rx/core/operators/onerrorresumenext.py
+++ rx/core/operators/onerrorresumenext.py
@@ -1,50 +1,10 @@
-from rx.core import Observable, ObservableBase, AnonymousObservable
-from rx.disposables import CompositeDisposable, SingleAssignmentDisposable, \
-    SerialDisposable
-from rx.internal.utils import is_future
+from typing import Callable
+
+import rx
+from rx.core import Observable
 
 
-def on_error_resume_next(*args) -> ObservableBase:
-    """Continues an observable sequence that is terminated normally or by
-    an exception with the next observable sequence.
-
-    1 - res = Observable.on_error_resume_next(xs, ys, zs)
-    2 - res = Observable.on_error_resume_next([xs, ys, zs])
-
-    Returns an observable sequence that concatenates the source sequences,
-    even if a sequence terminates exceptionally.
-    """
-    # curently not in:
-    # 3 - res = Observable.on_error_resume_next(xs, factory)
-
-    if args and isinstance(args[0], list):
-        sources = iter(args[0])
-    else:
-        sources = iter(args)
-
-    def subscribe(observer, scheduler=None):
-        subscription = SerialDisposable()
-        cancelable = SerialDisposable()
-
-        def action(scheduler, state=None):
-            try:
-                source = next(sources)
-            except StopIteration:
-                observer.on_completed()
-                return
-
-            # Allow source to be a factory method taking an error
-            source = source(state) if callable(source) else source
-            current = Observable.from_future(source) if is_future(source) else source
-
-            d = SingleAssignmentDisposable()
-            subscription.disposable = d
-
-            def on_resume(state=None):
-                scheduler.schedule(action, state)
-
-            d.disposable = current.subscribe_(observer.on_next, on_resume, on_resume, scheduler)
-
-        cancelable.disposable = scheduler.schedule(action)
-        return CompositeDisposable(subscription, cancelable)
-    return AnonymousObservable(subscribe)
+def _on_error_resume_next(second) -> Callable[[Observable], Observable]:
+    def on_error_resume_next(source: Observable) -> Observable:
+        return rx.on_error_resume_next([source, second])
+    return on_error_resume_next
