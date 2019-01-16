7c682a893e7ddcfeb49d25550120a23f2282bfd8
Dag Brattli <dag@brattli.net>
2019-01-16 07:38:01 +0100

    Fixes to_future

--- rx/core/operators/tofuture.py
+++ rx/core/operators/tofuture.py
@@ -4,33 +4,37 @@ from asyncio import Future
 from rx.core import Observable
 
 
-def to_future(source: Observable, future_ctor: Callable[[], Future] = None) -> Future:
-    """Converts an existing observable sequence to a Future.
-
-    Example:
-    future = rx.Observable.return_value(42).to_future(asyncio.Future);
-
-    future_ctor -- [Optional] The constructor of the future.
-
-    Returns a future with the last value from the observable sequence.
-    """
-
+def _to_future(future_ctor: Callable[[], Future] = None) -> Callable[[Observable], Future]:
     future_ctor = future_ctor or Future
     future = future_ctor()
 
-    has_value = []
+    def to_future(source: Observable) -> Future:
+        """Converts an existing observable sequence to a Future.
 
-    def on_next(value):
-        has_value.append(value)
+        Example:
+            future = rx.return_value(42).pipe(ops.to_future(asyncio.Future))
 
-    def on_error(err):
-        future.set_exception(err)
+        Args:
+            future_ctor: [Optional] The constructor of the future.
 
-    def on_completed():
-        if has_value:
-            future.set_result(has_value.pop())
+        Returns:
+            A future with the last value from the observable sequence.
+        """
 
-    source.subscribe_(on_next, on_error, on_completed)
+        has_value = []
 
-    # No cancellation can be done
-    return future
+        def on_next(value):
+            has_value.append(value)
+
+        def on_error(err):
+            future.set_exception(err)
+
+        def on_completed():
+            if has_value:
+                future.set_result(has_value.pop())
+
+        source.subscribe_(on_next, on_error, on_completed)
+
+        # No cancellation can be done
+        return future
+    return to_future
--- tests/test_observable/test_tofuture.py
+++ tests/test_observable/test_tofuture.py
@@ -1,6 +1,48 @@
-from nose import SkipTest
+
+import sys
+import unittest
+import asyncio
+
 
 import rx
-# set also in py2, when e.g. trollius is available:
+from rx.core import Observable, Disposable
+from rx.testing import TestScheduler, ReactiveTest
+from rx.disposables import SerialDisposable
 
-from .py2_3_tofuture import *
+on_next = ReactiveTest.on_next
+on_completed = ReactiveTest.on_completed
+on_error = ReactiveTest.on_error
+subscribe = ReactiveTest.subscribe
+subscribed = ReactiveTest.subscribed
+disposed = ReactiveTest.disposed
+created = ReactiveTest.created
+
+
+class TestToFuture(unittest.TestCase):
+    def test_await_success(self):
+        loop = asyncio.get_event_loop()
+        result = None
+
+        async def go():
+            nonlocal result
+            source = rx.return_value(42)
+            result = await source
+
+        loop.run_until_complete(go())
+        assert result == 42
+
+    def test_await_error(self):
+        loop = asyncio.get_event_loop()
+        error = Exception("error")
+        result = None
+
+        async def go():
+            nonlocal result
+            source = rx.throw(error)
+            try:
+                result = await source
+            except Exception as ex:
+                result = ex
+
+        loop.run_until_complete(go())
+        assert result == error
