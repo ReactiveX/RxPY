ef1400d7eed8c57e6cbc31c5990b205d5479eb9e
Dag Brattli <dag@brattli.net>
2019-01-17 09:33:12 +0100

    Fixed retry

--- rx/core/operators/retry.py
+++ rx/core/operators/retry.py
@@ -1,20 +1,29 @@
-from rx.core import Observable, ObservableBase
-from rx.internal.iterable import Iterable
+import sys
+from typing import Callable
+
+import rx
+from rx.core import Observable
 
 
-def retry(self, retry_count: int = None) -> ObservableBase:
-    """Repeats the source observable sequence the specified number of times
-    or until it successfully terminates. If the retry count is not
-    specified, it retries indefinitely.
+def _retry(retry_count: int = None) -> Callable[[Observable], Observable]:
+    """Repeats the source observable sequence the specified number of
+    times or until it successfully terminates. If the retry count is
+    not specified, it retries indefinitely.
 
-    1 - retried = xs.retry()
-    2 - retried = xs.retry(42)
+    Examples:
+        >>> retried = retry()
+        >>> retried = retry(42)
 
-    retry_count -- [Optional] Number of times to retry the sequence. If
-    not provided, retry the sequence indefinitely.
+    Args:
+        retry_count: [Optional] Number of times to retry the sequence.
+            If not provided, retry the sequence indefinitely.
 
-    Returns an observable sequence producing the elements of the given
-    sequence repeatedly until it terminates successfully.
+    Returns:
+        An observable sequence producing the elements of the given
+        sequence repeatedly until it terminates successfully.
     """
 
-    return Observable.catch_exception(Iterable.repeat(self, retry_count))
+
+    def retry(source: Observable) -> Observable:
+        return rx.catch_exception(source for sourc in range(retry_count or sys.maxsize))
+    return retry
