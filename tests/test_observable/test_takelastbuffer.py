24f2fa1ffc82a6ce343df8d06c80e04ece51bc02
Jérémie Fache <jeremie.fache@orange.fr>
2019-01-17 19:01:55 +0100

    update tests (pipestyle) for skip_with_time, take_last_buffer, take_last_with_time

--- tests/test_observable/test_takelastbuffer.py
+++ tests/test_observable/test_takelastbuffer.py
@@ -1,5 +1,6 @@
 import unittest
 
+from rx import operators as ops
 from rx.testing import TestScheduler, ReactiveTest
 
 on_next = ReactiveTest.on_next
@@ -15,10 +16,14 @@ class TestTakeLastBuffer(unittest.TestCase):
 
     def test_take_last_buffer_zero_completed(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9), on_completed(650))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9),
+                on_completed(650))
 
         def create():
-            return xs.take_last_buffer(0)
+            return xs.pipe(ops.take_last_buffer(0))
 
         res = scheduler.start(create)
 
@@ -31,10 +36,14 @@ class TestTakeLastBuffer(unittest.TestCase):
     def test_take_last_buffer_zero_error(self):
         ex = 'ex'
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9), on_error(650, ex))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9),
+                on_error(650, ex))
 
         def create():
-            return xs.take_last_buffer(0)
+            return xs.pipe(ops.take_last_buffer(0))
 
         res = scheduler.start(create)
 
@@ -43,10 +52,13 @@ class TestTakeLastBuffer(unittest.TestCase):
 
     def test_take_last_buffer_zero_disposed(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9))
 
         def create():
-            return xs.take_last_buffer(0)
+            return xs.pipe(ops.take_last_buffer(0))
 
         res = scheduler.start(create)
 
@@ -55,10 +67,14 @@ class TestTakeLastBuffer(unittest.TestCase):
 
     def test_take_last_buffer_one_completed(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9), on_completed(650))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9),
+                on_completed(650))
 
         def create():
-            return xs.take_last_buffer(1)
+            return xs.pipe(ops.take_last_buffer(1))
 
         res = scheduler.start(create)
 
@@ -71,10 +87,14 @@ class TestTakeLastBuffer(unittest.TestCase):
     def test_take_last_buffer_one_error(self):
         ex = 'ex'
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9), on_error(650, ex))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9),
+                on_error(650, ex))
 
         def create():
-            return xs.take_last_buffer(1)
+            return xs.pipe(ops.take_last_buffer(1))
 
         res = scheduler.start(create)
 
@@ -84,10 +104,13 @@ class TestTakeLastBuffer(unittest.TestCase):
     def test_take_last_buffer_one_disposed(self):
         scheduler = TestScheduler()
 
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9))
 
         def create():
-            return xs.take_last_buffer(1)
+            return xs.pipe(ops.take_last_buffer(1))
 
         res = scheduler.start(create)
 
@@ -96,10 +119,14 @@ class TestTakeLastBuffer(unittest.TestCase):
 
     def test_take_last_buffer_three_completed(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9), on_completed(650))
+        xs = scheduler.create_hot_observable(
+                on_next(180, 1), on_next(210, 2), on_next(250, 3),
+                on_next(270, 4), on_next(310, 5), on_next(360, 6),
+                on_next(380, 7), on_next(410, 8), on_next(590, 9),
+                on_completed(650))
 
         def create():
-            return xs.take_last_buffer(3)
+            return xs.pipe(ops.take_last_buffer(3))
 
         res = scheduler.start(create)
 
@@ -115,7 +142,7 @@ class TestTakeLastBuffer(unittest.TestCase):
 #     scheduler = TestScheduler()
 #     xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9), on_error(650, ex))
 #     res = scheduler.start(create)
-#         return xs.take_last_buffer(3)
+#         return xs.pipe(ops.take_last_buffer(3))
 
 #     assert res.messages == [on_error(650, ex)]
 #     assert xs.subscriptions == [subscribe(200, 650)]
@@ -125,7 +152,7 @@ class TestTakeLastBuffer(unittest.TestCase):
 #     scheduler = TestScheduler()
 #     xs = scheduler.create_hot_observable(on_next(180, 1), on_next(210, 2), on_next(250, 3), on_next(270, 4), on_next(310, 5), on_next(360, 6), on_next(380, 7), on_next(410, 8), on_next(590, 9))
 #     res = scheduler.start(create)
-#         return xs.take_last_buffer(3)
+#         return xs.pipe(ops.take_last_buffer(3))
 
 #     assert res.messages == []
 #     assert xs.subscriptions == [subscribe(200, 1000)]
--- tests/test_observable/test_takelastwithtime.py
+++ tests/test_observable/test_takelastwithtime.py
@@ -1,5 +1,6 @@
 import unittest
 
+from rx import operators as ops
 from rx.testing import TestScheduler, ReactiveTest
 
 on_next = ReactiveTest.on_next
@@ -20,7 +21,7 @@ class TestTakeLast(unittest.TestCase):
             on_completed(230))
 
         def create():
-            return xs.take_last_with_time(0)
+            return xs.pipe(ops.take_last_with_time(0))
 
         res = scheduler.start(create)
 
@@ -29,10 +30,12 @@ class TestTakeLast(unittest.TestCase):
 
     def test_takelast_zero2(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(210, 1), on_next(220, 2), on_next(230, 3), on_completed(230))
+        xs = scheduler.create_hot_observable(
+                on_next(210, 1), on_next(220, 2), on_next(230, 3),
+                on_completed(230))
 
         def create():
-            return xs.take_last_with_time(0)
+            return xs.pipe(ops.take_last_with_time(0))
 
         res = scheduler.start(create)
 
@@ -41,10 +44,12 @@ class TestTakeLast(unittest.TestCase):
 
     def test_takelast_some1(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(210, 1), on_next(220, 2), on_next(230, 3), on_completed(240))
+        xs = scheduler.create_hot_observable(
+                on_next(210, 1), on_next(220, 2), on_next(230, 3),
+                on_completed(240))
 
         def create():
-            return xs.take_last_with_time(25)
+            return xs.pipe(ops.take_last_with_time(25))
 
         res = scheduler.start(create)
 
@@ -53,10 +58,12 @@ class TestTakeLast(unittest.TestCase):
 
     def test_takelast_some2(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(210, 1), on_next(220, 2), on_next(230, 3), on_completed(300))
+        xs = scheduler.create_hot_observable(
+                on_next(210, 1), on_next(220, 2), on_next(230, 3),
+                on_completed(300))
 
         def create():
-            return xs.take_last_with_time(25)
+            return xs.pipe(ops.take_last_with_time(25))
 
         res = scheduler.start(create)
 
@@ -65,22 +72,31 @@ class TestTakeLast(unittest.TestCase):
 
     def test_takelast_some3(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(210, 1), on_next(220, 2), on_next(230, 3), on_next(240, 4), on_next(250, 5), on_next(260, 6), on_next(270, 7), on_next(280, 8), on_next(290, 9), on_completed(300))
+        xs = scheduler.create_hot_observable(
+                on_next(210, 1), on_next(220, 2), on_next(230, 3),
+                on_next(240, 4), on_next(250, 5), on_next(260, 6),
+                on_next(270, 7), on_next(280, 8), on_next(290, 9),
+                on_completed(300))
 
         def create():
-            return xs.take_last_with_time(45)
+            return xs.pipe(ops.take_last_with_time(45))
 
         res = scheduler.start(create)
 
-        assert res.messages == [on_next(300, 6), on_next(300, 7), on_next(300, 8), on_next(300, 9), on_completed(300)]
+        assert res.messages == [
+                on_next(300, 6), on_next(300, 7), on_next(300, 8),
+                on_next(300, 9), on_completed(300)]
         assert xs.subscriptions == [subscribe(200, 300)]
 
     def test_takelast_some4(self):
         scheduler = TestScheduler()
-        xs = scheduler.create_hot_observable(on_next(210, 1), on_next(240, 2), on_next(250, 3), on_next(280, 4), on_next(290, 5), on_next(300, 6), on_completed(350))
+        xs = scheduler.create_hot_observable(
+                on_next(210, 1), on_next(240, 2), on_next(250, 3),
+                on_next(280, 4), on_next(290, 5), on_next(300, 6),
+                on_completed(350))
 
         def create():
-            return xs.take_last_with_time(25)
+            return xs.pipe(ops.take_last_with_time(25))
 
         res = scheduler.start(create)
 
@@ -92,7 +108,7 @@ class TestTakeLast(unittest.TestCase):
         xs = scheduler.create_hot_observable(on_next(210, 1), on_next(220, 2), on_completed(230))
 
         def create():
-            return xs.take_last_with_time(50)
+            return xs.pipe(ops.take_last_with_time(50))
 
         res = scheduler.start(create)
 
@@ -105,7 +121,7 @@ class TestTakeLast(unittest.TestCase):
         xs = scheduler.create_hot_observable(on_error(210, ex))
 
         def create():
-            return xs.take_last_with_time(50)
+            return xs.pipe(ops.take_last_with_time(50))
 
         res = scheduler.start(create)
 
@@ -117,7 +133,7 @@ class TestTakeLast(unittest.TestCase):
         xs = scheduler.create_hot_observable()
 
         def create():
-            return xs.take_last_with_time(50)
+            return xs.pipe(ops.take_last_with_time(50))
 
         res = scheduler.start(create)
 
