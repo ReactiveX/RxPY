db637097508be4214aec55c84b9a9edebaad3f05
Dag Brattli <dag@brattli.net>
2019-01-18 09:26:08 +0100

    Fixed skips

--- rx/core/operators/skipuntilwithtime.py
+++ rx/core/operators/skipuntilwithtime.py
@@ -1,47 +1,51 @@
 from datetime import datetime
-from typing import Union
+from typing import Union, Callable
 
-from rx.core import ObservableBase, AnonymousObservable
+from rx.core import Observable, AnonymousObservable
 from rx.disposables import CompositeDisposable
 from rx.concurrency import timeout_scheduler
 
 
-def skip_until_with_time(source: ObservableBase,
-                         start_time: Union[datetime, int]) -> ObservableBase:
-    """Skips elements from the observable source sequence until the
-    specified start time.
-    Errors produced by the source sequence are always forwarded to the
-    result sequence, even if the error occurs before the start time.
+def _skip_until_with_time(start_time: Union[datetime, int]) -> Callable[[Observable], Observable]:
+    def skip_until_with_time(source: Observable) -> Observable:
+        """Skips elements from the observable source sequence until the
+        specified start time.
 
-    Examples:
-    res = source.skip_until_with_time(datetime);
-    res = source.skip_until_with_time(5000);
+        Errors produced by the source sequence are always forwarded to
+        the result sequence, even if the error occurs before the start
+        time.
 
-    Keyword arguments:
-    start_time -- Time to start taking elements from the source
-        sequence. If this value is less than or equal to Date(), no
-        elements will be skipped.
+        Examples:
+            >>> res = source.skip_until_with_time(datetime)
+            >>> res = source.skip_until_with_time(5000)
 
-    Returns an observable sequence with the elements skipped
-    until the specified start time.
-    """
+        Args:
+            start_time -- Time to start taking elements from the source
+                sequence. If this value is less than or equal to
+                `datetime.utcnow`, no elements will be skipped.
 
-    if isinstance(start_time, datetime):
-        scheduler_method = 'schedule_absolute'
-    else:
-        scheduler_method = 'schedule_relative'
+        Returns:
+            An observable sequence with the elements skipped until the
+            specified start time.
+        """
 
-    def subscribe(observer, scheduler=None):
-        scheduler = scheduler or timeout_scheduler
-        open = [False]
+        if isinstance(start_time, datetime):
+            scheduler_method = 'schedule_absolute'
+        else:
+            scheduler_method = 'schedule_relative'
 
-        def on_next(x):
-            if open[0]:
-                observer.on_next(x)
-        subscription = source.subscribe_(on_next, observer.on_error, observer.on_completed, scheduler)
+        def subscribe(observer, scheduler=None):
+            scheduler = scheduler or timeout_scheduler
+            open = [False]
 
-        def action(scheduler, state):
-            open[0] = True
-        disposable = getattr(scheduler, scheduler_method)(start_time, action)
-        return CompositeDisposable(disposable, subscription)
-    return AnonymousObservable(subscribe)
+            def on_next(x):
+                if open[0]:
+                    observer.on_next(x)
+            subscription = source.subscribe_(on_next, observer.on_error, observer.on_completed, scheduler)
+
+            def action(scheduler, state):
+                open[0] = True
+            disposable = getattr(scheduler, scheduler_method)(start_time, action)
+            return CompositeDisposable(disposable, subscription)
+        return AnonymousObservable(subscribe)
+    return skip_until_with_time
