94abf875fac102cc08e2f2acc647e23fe7a62bc9
Dag Brattli <dag@brattli.net>
2019-01-19 09:45:39 +0100

    Remove interable impl.

--- rx/internal/__init__.py
+++ rx/internal/__init__.py
@@ -1,6 +1,4 @@
 from .priorityqueue import PriorityQueue
 from .basic import noop, default_error, default_comparer
 from .exceptions import SequenceContainsNoElementsError, ArgumentOutOfRangeException, DisposedException
-from .iterable import Iterable
-from .anonymousiterable import AnonymousIterable
 from . import concurrency
--- rx/internal/anonymousiterable.py
+++
@@ -1,9 +0,0 @@
-from .iterable import Iterable
-
-
-class AnonymousIterable(Iterable):
-    def __init__(self, iterator):
-        self._iterator = iterator
-
-    def __iter__(self):
-        return self._iterator
