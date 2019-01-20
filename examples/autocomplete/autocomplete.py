99efc9106c7c78616da89f099e5336a7fd9b815d
Dag Brattli <dag@brattli.net>
2019-01-20 14:39:49 +0100

    Use ops instead of _ for examples

--- examples/autocomplete/autocomplete.py
+++ examples/autocomplete/autocomplete.py
@@ -15,7 +15,7 @@ from tornado.httputil import url_concat
 from tornado.escape import json_decode
 from tornado import ioloop
 
-from rx import operators as _
+from rx import operators as ops
 from rx.subjects import Subject
 from rx.concurrency import IOLoopScheduler
 
@@ -50,11 +50,11 @@ class WSHandler(WebSocketHandler):
 
         # Get all distinct key up events from the input and only fire if long enough and distinct
         searcher = self.stream.pipe(
-            _.map(lambda x: x["term"]),
-            _.filter(lambda text: len(text) > 2),  # Only if the text is longer than 2 characters
-            _.debounce(0.750),                     # Pause for 750ms
-            _.distinct_until_changed(),            # Only if the value has changed
-            _.flat_map_latest(search_wikipedia)
+            ops.map(lambda x: x["term"]),
+            ops.filter(lambda text: len(text) > 2),  # Only if the text is longer than 2 characters
+            ops.debounce(0.750),                     # Pause for 750ms
+            ops.distinct_until_changed(),            # Only if the value has changed
+            ops.flat_map_latest(search_wikipedia)
         )
 
         def send_response(x):
