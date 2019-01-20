60f30ca686b5e7610973ce6810510c22cb8e07f0
Dag Brattli <dag@brattli.net>
2019-01-20 14:00:33 +0100

    Remove observer extension from init

--- rx/core/__init__.py
+++ rx/core/__init__.py
@@ -1,10 +1,9 @@
 # flake8: noqa
 from .typing import Observer, Scheduler
-
 from .disposable import Disposable
+
 from .anonymousobserver import AnonymousObserver
 
-from . import observerextensions
 from .pipe import pipe
 
 from .observable import Observable
