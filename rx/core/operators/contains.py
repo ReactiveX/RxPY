15d7a39461a40eb87904d644ef201d9058735df5
Dag Brattli <dag@brattli.net>
2019-01-24 09:31:06 +0100

    Make count and contains operators point-free

--- rx/core/operators/contains.py
+++ rx/core/operators/contains.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable
 
 from rx import operators as ops
-from rx.core import Observable
+from rx.core import Observable, pipe
 from rx.internal.basic import default_comparer
 
 
@@ -11,24 +11,4 @@ def _contains(value: Any, comparer=None) -> Callable[[Observable], Observable]:
     filtering = ops.filter(lambda v: comparer_(v, value))
     something = ops.some()
 
-    def contains(source: Observable) -> Observable:
-        """Determines whether an observable sequence contains a specified
-        element with an optional equality comparer.
-
-        Examples:
-            >>> res = contains(sournce)
-
-        Args:
-            source: The source observable to compare.
-
-        Returns:
-            An observable sequence containing a single element
-            determining whether the source sequence contains an element
-            that has the specified value.
-        """
-
-        return source.pipe(
-            filtering,
-            something
-        )
-    return contains
+    return pipe(filtering, something)
--- rx/core/operators/count.py
+++ rx/core/operators/count.py
@@ -1,36 +1,15 @@
 from typing import Callable
-from rx.core import Observable
+from rx.core import Observable, pipe
 from rx.core.typing import Predicate
 
-from rx import operators as _
+from rx import operators as ops
 
 
 def _count(predicate: Predicate = None) -> Callable[[Observable], Observable]:
-    """Returns an observable sequence containing a value that represents
-    how many elements in the specified observable sequence satisfy a
-    condition if provided, else the count of items.
 
-    Examples:
-        >>> res = count()
-        >>> res = count(lambda x: x > 3)
+    if predicate:
+        filtering = ops.filter(predicate)
+        return pipe(filtering, ops.count())
 
-    Args:
-        predicate -- A function to test each element for a condition.
-
-    Returns:
-        An operator function that takes an observable source and returns
-        an observable sequence containing a single element with a number
-        that represents how many elements in the input sequence satisfy
-        the condition in the predicate function if provided, else
-        the count of items in the sequence.
-    """
-
-    counter = _.reduce(lambda n, _: n + 1, seed=0)
-
-    def count(source: Observable) -> Observable:
-        if predicate:
-            filtering = _.filter(predicate)
-            return source.pipe(filtering, _.count())
-
-        return source.pipe(counter)
-    return count
+    counter = ops.reduce(lambda n, _: n + 1, seed=0)
+    return pipe(counter)
