f1894122590b1368033d8886f79f2d05edb62e91
Erik Kemperman <erikkemperman@gmail.com>
2019-02-18 10:43:28 +0100

    Cosmetic: assert is not a fun call

--- tests/test_core/test_observer.py
+++ tests/test_core/test_observer.py
@@ -3,6 +3,7 @@ from rx.core.notification import OnNext, OnError, OnCompleted, from_notifier
 
 
 class MyObserver(Observer):
+
     def __init__(self):
         super().__init__()
         self.has_on_next = None
@@ -23,11 +24,11 @@ def test_to_observer_notification_on_next():
     i = 0
 
     def next(n):
-        assert(i == 0)
-        assert(n.kind == 'N')
-        assert(n.value == 42)
-        assert(not hasattr(n, "exception"))
-        assert(n.has_value)
+        assert i == 0
+        assert n.kind == 'N'
+        assert n.value == 42
+        assert not hasattr(n, "exception")
+        assert n.has_value
 
     from_notifier(next).on_next(42)
 
@@ -37,10 +38,10 @@ def test_to_observer_notification_on_error():
     i = 0
 
     def next(n):
-        assert(i == 0)
-        assert(n.kind == 'E')
-        assert(n.exception == ex)
-        assert(not n.has_value)
+        assert i == 0
+        assert n.kind == 'E'
+        assert n.exception == ex
+        assert not n.has_value
 
     from_notifier(next).on_error(ex)
 
@@ -49,9 +50,9 @@ def test_to_observer_notification_completed():
     i = 0
 
     def next(n):
-        assert(i == 0)
-        assert(n.kind == 'C')
-        assert(not n.has_value)
+        assert i == 0
+        assert n.kind == 'C'
+        assert not n.has_value
 
     from_notifier(next).on_completed()
 
@@ -59,29 +60,29 @@ def test_to_observer_notification_completed():
 def test_to_notifier_forwards():
     obsn = MyObserver()
     obsn.to_notifier()(OnNext(42))
-    assert(obsn.has_on_next == 42)
+    assert obsn.has_on_next == 42
 
     ex = 'ex'
     obse = MyObserver()
     obse.to_notifier()(OnError(ex))
-    assert(ex == obse.has_on_error)
+    assert ex == obse.has_on_error
 
     obsc = MyObserver()
     obsc.to_notifier()(OnCompleted())
-    assert(obsc.has_on_completed)
+    assert obsc.has_on_completed
 
 
 def test_create_on_next():
     next = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     res = Observer(on_next)
 
     res.on_next(42)
-    assert(next[0])
+    assert next[0]
     return res.on_completed()
 
 
@@ -91,21 +92,21 @@ def test_create_on_next_has_error():
     _e = None
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     res = Observer(on_next)
 
     res.on_next(42)
-    assert(next[0])
+    assert next[0]
 
     try:
         res.on_error(ex)
-        assert(False)
+        assert False
     except Exception as e:
         e_ = e.args[0]
 
-    assert(ex == e_)
+    assert ex == e_
 
 
 def test_create_on_next_on_completed():
@@ -113,7 +114,7 @@ def test_create_on_next_on_completed():
     completed = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
         return next[0]
 
@@ -125,12 +126,12 @@ def test_create_on_next_on_completed():
 
     res.on_next(42)
 
-    assert(next[0])
-    assert(not completed[0])
+    assert next[0]
+    assert not completed[0]
 
     res.on_completed()
 
-    assert(completed[0])
+    assert completed[0]
 
 
 def test_create_on_next_close_has_error():
@@ -140,7 +141,7 @@ def test_create_on_next_close_has_error():
     completed = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     def on_completed():
@@ -149,16 +150,16 @@ def test_create_on_next_close_has_error():
     res = Observer(on_next, None, on_completed)
 
     res.on_next(42)
-    assert(next[0])
-    assert(not completed[0])
+    assert next[0]
+    assert not completed[0]
     try:
         res.on_error(ex)
-        assert(False)
+        assert False
     except Exception as e:
         e_ = e.args[0]
 
-    assert(ex == e_)
-    assert(not completed[0])
+    assert ex == e_
+    assert not completed[0]
 
 
 def test_create_on_next_on_error():
@@ -167,22 +168,22 @@ def test_create_on_next_on_error():
     error = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     def on_error(e):
-        assert(ex == e)
+        assert ex == e
         error[0] = True
 
     res = Observer(on_next, on_error)
 
     res.on_next(42)
 
-    assert(next[0])
-    assert(not error[0])
+    assert next[0]
+    assert not error[0]
 
     res.on_error(ex)
-    assert(error[0])
+    assert error[0]
 
 
 def test_create_on_next_throw_hit_completed():
@@ -191,22 +192,22 @@ def test_create_on_next_throw_hit_completed():
     error = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     def on_error(e):
-        assert(ex == e)
+        assert ex == e
         error[0] = True
 
     res = Observer(on_next, on_error)
 
     res.on_next(42)
-    assert(next[0])
-    assert(not error[0])
+    assert next[0]
+    assert not error[0]
 
     res.on_completed()
 
-    assert(not error[0])
+    assert not error[0]
 
 
 def test_create_on_next_throw_close1():
@@ -216,11 +217,11 @@ def test_create_on_next_throw_close1():
     completed = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     def on_error(e):
-        assert(ex == e)
+        assert ex == e
         error[0] = True
 
     def on_completed():
@@ -230,14 +231,14 @@ def test_create_on_next_throw_close1():
 
     res.on_next(42)
 
-    assert(next[0])
-    assert(not error[0])
-    assert(not completed[0])
+    assert next[0]
+    assert not error[0]
+    assert not completed[0]
 
     res.on_completed()
 
-    assert(completed[0])
-    assert(not error[0])
+    assert completed[0]
+    assert not error[0]
 
 
 def test_create_on_next_throw_close2():
@@ -247,11 +248,11 @@ def test_create_on_next_throw_close2():
     completed = [False]
 
     def on_next(x):
-        assert(42 == x)
+        assert 42 == x
         next[0] = True
 
     def on_error(e):
-        assert(ex == e)
+        assert ex == e
         error[0] = True
 
     def on_completed():
@@ -261,37 +262,38 @@ def test_create_on_next_throw_close2():
 
     res.on_next(42)
 
-    assert(next[0])
-    assert(not error[0])
-    assert(not completed[0])
+    assert next[0]
+    assert not error[0]
+    assert not completed[0]
 
     res.on_error(ex)
 
-    assert(not completed[0])
-    assert(error[0])
+    assert not completed[0]
+    assert error[0]
 
 
 def test_as_observer_hides():
     obs = MyObserver()
     res = obs.as_observer()
 
-    assert(res != obs)
-    assert(not isinstance(res, obs.__class__))
+    assert res != obs
+    assert not isinstance(res, obs.__class__)
 
 
 def test_as_observer_forwards():
     obsn = MyObserver()
     obsn.as_observer().on_next(42)
-    assert(obsn.has_on_next == 42)
+    assert obsn.has_on_next == 42
 
     ex = 'ex'
     obse = MyObserver()
     obse.as_observer().on_error(ex)
-    assert(obse.has_on_error == ex)
+    assert obse.has_on_error == ex
 
     obsc = MyObserver()
     obsc.as_observer().on_completed()
-    assert(obsc.has_on_completed)
+    assert obsc.has_on_completed
+
 
 if __name__ == '__main__':
     test_to_notifier_forwards()
