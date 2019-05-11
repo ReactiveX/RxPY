027c8d6774d9a990d2b44b7bcca6d6c8b2f39da7
Dag Brattli <dag@brattli.net>
2019-05-11 10:43:56 +0200

    Revert "Add an Observable specific overload first with docstring to make pylint happy."
    This reverts commit b9fb57889087baf5de5765c1396a4421e074184a.

--- rx/core/observable/observable.py
+++ rx/core/observable/observable.py
@@ -216,25 +216,7 @@ class Observable(typing.Observable):
         return Disposable(auto_detach_observer.dispose)
 
     @overload
-    def pipe(self, *operators: Callable[['Observable'], 'Observable']) -> 'Observable':  # pylint: disable=no-self-use
-        """Compose multiple operators left to right.
-
-        Composes zero or more operators into a functional composition.
-        The operators are composed to right. A composition of zero
-        operators gives back the original source.
-
-        source.pipe() == source
-        source.pipe(f) == f(source)
-        source.pipe(g, f) == f(g(source))
-        source.pipe(h, g, f) == f(g(h(source)))
-        ...
-
-        Returns the composed observable.
-        """
-        ...
-
-    @overload
-    def pipe(self) -> 'Observable':  # pylint: disable=function-redefined, no-self-use
+    def pipe(self) -> 'Observable':  # pylint: disable=no-self-use
         ...  # pylint: disable=pointless-statement
 
     @overload
@@ -282,7 +264,7 @@ class Observable(typing.Observable):
         ...  # pylint: disable=pointless-statement
 
     @overload
-    def pipe(self,  # pylint: disable=function-redefined, no-self-use, too-many-arguments
+    def pipe(self,  # pylint: disable=function-redefined,too-many-arguments,no-self-use
              op1: Callable[['Observable'], A],
              op2: Callable[[A], B],
              op3: Callable[[B], C],
@@ -293,7 +275,7 @@ class Observable(typing.Observable):
         ...  # pylint: disable=pointless-statement
 
     # pylint: disable=function-redefined
-    def pipe(self, *operators: Callable[['Observable'], Any]) -> Any:
+    def pipe(self, *operators: Callable[['Observable'], Any]) -> Any:  # type: ignore
         """Compose multiple operators left to right.
 
         Composes zero or more operators into a functional composition.
