497e674a87902249493379464239cae385670229
Erik Kemperman <erikkemperman@gmail.com>
2019-05-14 15:06:21 +0200

    Use MIT license

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Other Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
