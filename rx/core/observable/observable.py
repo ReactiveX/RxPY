26d9a3d0cfe7610de1f94dfa1e47424e3a328169
Erik Kemperman <erikkemperman@gmail.com>
2019-06-07 12:22:06 +0200

    Sphinx apidocs: fix crosslink issues, polish rx.Observable (#419)
    * Remove 'copyright' (ends up double in generated docs)
    
    * Add docstrings to core.typing, include it in API reference
    
    * Fix crosslink issues in sphinx apidocs
    
    * Polish docstring for rx.core.observable.Observable
    
    * Re-order methods in Observable for nicer apidoc

--- rx/core/observable/observable.py
+++ rx/core/observable/observable.py
@@ -20,97 +20,26 @@ G = TypeVar('G')
 class Observable(typing.Observable):
     """Observable base class.
 
-    Represents a push-style collection and contains all operators as
-    methods to allow classic Rx chaining of operators."""
+    Represents a push-style collection, which you can :func:`pipe <pipe>` into
+    :mod:`operators <rx.operators>`."""
 
-    def __init__(self, subscribe: Optional[Callable[[typing.Observer, Optional[typing.Scheduler]], typing.Disposable]] = None) -> None:
+    def __init__(self, subscribe: Optional[typing.Subscribable] = None) -> None:
         """Creates an observable sequence object from the specified
         subscription function.
 
         Args:
-            subscribe: Subscribe method implementation.
+            subscribe: [Optional] Subscription function
         """
+        super().__init__()
 
         self.lock = threading.RLock()
         self._subscribe = subscribe
 
-        super().__init__()
-
-    def _subscribe_core(self, observer: typing.Observer, scheduler: typing.Scheduler = None):
+    def _subscribe_core(self,
+                        observer: typing.Observer,
+                        scheduler: Optional[typing.Scheduler] = None
+                        ) -> typing.Disposable:
         return self._subscribe(observer, scheduler) if self._subscribe else Disposable()
-
-    def __await__(self) -> Any:
-        """Awaits the given observable.
-
-        Returns:
-            The last item of the observable sequence.
-
-        Raises:
-            TypeError: If key is not of type int or slice
-        """
-        from ..operators.tofuture import _to_future
-        return iter(self.pipe(_to_future()))
-
-    def __add__(self, other):
-        """Pythonic version of concat.
-
-        Example:
-            >>> zs = xs + ys
-
-        Returns:
-            self.concat(other)"""
-        from rx import concat
-        return concat(self, other)
-
-    def __getitem__(self, key):
-        """Slices the given observable using Python slice notation. The
-        arguments to slice is start, stop and step given within
-        brackets [] and separated with the ':' character. It is
-        basically a wrapper around the operators skip(), skip_last(),
-        take(), take_last() and filter().
-
-        This marble diagram helps you remember how slices works with
-        streams. Positive numbers is relative to the start of the
-        events, while negative numbers are relative to the end (close)
-        of the stream.
-
-        r---e---a---c---t---i---v---e---|
-        0   1   2   3   4   5   6   7   8
-       -8  -7  -6  -5  -4  -3  -2  -1   0
-
-        Examples:
-            >>> result = source[1:10]
-            >>> result = source[1:-2]
-            >>> result = source[1:-1:2]
-
-        Args:
-            key: Slice object
-
-        Returns:
-            A sliced observable sequence.
-        """
-
-        if isinstance(key, slice):
-            start, stop, step = key.start, key.stop, key.step
-        elif isinstance(key, int):
-            start, stop, step = key, key + 1, 1
-        else:
-            raise TypeError("Invalid argument type.")
-
-        from ..operators.slice import _slice
-        return _slice(start, stop, step)(self)
-
-    def __iadd__(self, other):
-        """Pythonic use of concat.
-
-        Example:
-            xs += ys
-
-        Returns:
-            rx.concat(self, other)
-        """
-        from rx import concat
-        return concat(self, other)
 
     def subscribe(self,  # pylint: disable=too-many-arguments,arguments-differ
                   observer: Optional[Union[typing.Observer, typing.OnNext]] = None,
@@ -121,6 +50,12 @@ class Observable(typing.Observable):
                   scheduler: Optional[typing.Scheduler] = None,
                   ) -> typing.Disposable:
         """Subscribe an observer to the observable sequence.
+
+        You may subscribe using an observer or callbacks, not both; if the first
+        argument is an instance of :class:`Observer <..typing.Observer>` or if
+        it has a (callable) attribute named :code:`on_next`, then any callback
+        arguments will be ignored.
+
 
         Examples:
             >>> source.subscribe()
@@ -133,24 +68,29 @@ class Observable(typing.Observable):
 
         Args:
             observer: [Optional] The object that is to receive
-                notifications. You may subscribe using an observer or
-                callbacks, not both.
+                notifications.
+            on_error: [Optional] Action to invoke upon exceptional termination
+                of the observable sequence.
+            on_completed: [Optional] Action to invoke upon graceful termination
+                of the observable sequence.
+            on_next: [Optional] Action to invoke for each element in the
+                observable sequence.
             scheduler: [Optional] The default scheduler to use for this
                 subscription.
 
         Returns:
-            Disposable object representing an observer's subscription
-            to the observable sequence.
+            Disposable object representing an observer's subscription to
+            the observable sequence.
         """
-
         if observer:
-            if isinstance(observer, typing.Observer) or hasattr(observer, "on_next"):
+            if isinstance(observer, typing.Observer) \
+                    or hasattr(observer, 'on_next') \
+                    and callable(getattr(observer, 'on_next')):
                 on_next = cast(typing.Observer, observer).on_next
                 on_error = cast(typing.Observer, observer).on_error
                 on_completed = cast(typing.Observer, observer).on_completed
             else:
                 on_next = observer
-
         return self.subscribe_(on_next, on_error, on_completed, scheduler)
 
     def subscribe_(self,
@@ -167,17 +107,17 @@ class Observable(typing.Observable):
             >>> source.subscribe_(on_next, on_error, on_completed)
 
         Args:
-            on_next: Action to invoke for each element in the observable
-                sequence.
-            on_error: Action to invoke upon exceptional termination of
-                the observable sequence.
-            on_completed: Action to invoke upon graceful termination of
-                the observable sequence.
-            scheduler: The scheduler to use for this subscription.
+            on_next: [Optional] Action to invoke for each element in the
+                observable sequence.
+            on_error: [Optional] Action to invoke upon exceptional termination
+                of the observable sequence.
+            on_completed: [Optional] Action to invoke upon graceful termination
+                of the observable sequence.
+            scheduler: [Optional] The scheduler to use for this subscription.
 
         Returns:
-            Disposable object representing an observer's subscription
-            to the observable sequence.
+            Disposable object representing an observer's subscription to
+            the observable sequence.
         """
 
         auto_detach_observer = AutoDetachObserver(on_next, on_error, on_completed)
@@ -185,7 +125,7 @@ class Observable(typing.Observable):
         def fix_subscriber(subscriber):
             """Fixes subscriber to make sure it returns a Disposable instead
             of None or a dispose function"""
-            if not hasattr(subscriber, "dispose"):
+            if not hasattr(subscriber, 'dispose'):
                 subscriber = Disposable(subscriber)
 
             return subscriber
@@ -216,11 +156,13 @@ class Observable(typing.Observable):
         return Disposable(auto_detach_observer.dispose)
 
     @overload
-    def pipe(self, *operators: Callable[['Observable'], 'Observable']) -> 'Observable':  # pylint: disable=no-self-use
+    def pipe(self,
+             *operators: Callable[['Observable'], 'Observable']
+             ) -> 'Observable':  # pylint: disable=no-self-use
         """Compose multiple operators left to right.
 
         Composes zero or more operators into a functional composition.
-        The operators are composed to right. A composition of zero
+        The operators are composed from left to right. A composition of zero
         operators gives back the original source.
 
         Examples:
@@ -229,7 +171,11 @@ class Observable(typing.Observable):
             >>> source.pipe(g, f) == f(g(source))
             >>> source.pipe(h, g, f) == f(g(h(source)))
 
-        Returns the composed observable.
+        Args:
+            operators: Sequence of operators.
+
+        Returns:
+             The composed observable.
         """
         ...
 
@@ -297,16 +243,20 @@ class Observable(typing.Observable):
         """Compose multiple operators left to right.
 
         Composes zero or more operators into a functional composition.
-        The operators are composed to right. A composition of zero
+        The operators are composed from left to right. A composition of zero
         operators gives back the original source.
 
-        source.pipe() == source
-        source.pipe(f) == f(source)
-        source.pipe(g, f) == f(g(source))
-        source.pipe(h, g, f) == f(g(h(source)))
-        ...
+        Examples:
+            >>> source.pipe() == source
+            >>> source.pipe(f) == f(source)
+            >>> source.pipe(g, f) == f(g(source))
+            >>> source.pipe(h, g, f) == f(g(h(source)))
 
-        Returns the composed observable.
+        Args:
+            operators: Sequence of operators.
+
+        Returns:
+             The composed observable.
         """
         from ..pipe import pipe
         return pipe(*operators)(self)
@@ -331,3 +281,92 @@ class Observable(typing.Observable):
         """
         from ..run import run
         return run(self)
+
+    def __await__(self) -> Any:
+        """Awaits the given observable.
+
+        Returns:
+            The last item of the observable sequence.
+        """
+        from ..operators.tofuture import _to_future
+        return iter(self.pipe(_to_future()))
+
+    def __add__(self, other) -> 'Observable':
+        """Pythonic version of :func:`concat <rx.concat>`.
+
+        Example:
+            >>> zs = xs + ys
+
+        Args:
+            other: The second observable sequence in the concatenation.
+
+        Returns:
+            Concatenated observable sequence.
+        """
+        from rx import concat
+        return concat(self, other)
+
+    def __iadd__(self, other) -> 'Observable':
+        """Pythonic use of :func:`concat <rx.concat>`.
+
+        Example:
+            >>> xs += ys
+
+        Args:
+            other: The second observable sequence in the concatenation.
+
+        Returns:
+            Concatenated observable sequence.
+        """
+        from rx import concat
+        return concat(self, other)
+
+    def __getitem__(self, key) -> 'Observable':
+        """
+        Pythonic version of :func:`slice <rx.operators.slice>`.
+
+        Slices the given observable using Python slice notation. The arguments
+        to slice are `start`, `stop` and `step` given within brackets `[]` and
+        separated by the colons `:`.
+
+        It is basically a wrapper around the operators
+        :func:`skip <rx.operators.skip>`,
+        :func:`skip_last <rx.operators.skip_last>`,
+        :func:`take <rx.operators.take>`,
+        :func:`take_last <rx.operators.take_last>` and
+        :func:`filter <rx.operators.filter>`.
+
+        The following diagram helps you remember how slices works with streams.
+        Positive numbers are relative to the start of the events, while negative
+        numbers are relative to the end (close) of the stream.
+
+        .. code::
+
+            r---e---a---c---t---i---v---e---!
+            0   1   2   3   4   5   6   7   8
+           -8  -7  -6  -5  -4  -3  -2  -1   0
+
+        Examples:
+            >>> result = source[1:10]
+            >>> result = source[1:-2]
+            >>> result = source[1:-1:2]
+
+        Args:
+            key: Slice object
+
+        Returns:
+            Sliced observable sequence.
+
+        Raises:
+            TypeError: If key is not of type :code:`int` or :code:`slice`
+        """
+
+        if isinstance(key, slice):
+            start, stop, step = key.start, key.stop, key.step
+        elif isinstance(key, int):
+            start, stop, step = key, key + 1, 1
+        else:
+            raise TypeError('Invalid argument type.')
+
+        from ..operators.slice import _slice
+        return _slice(start, stop, step)(self)
