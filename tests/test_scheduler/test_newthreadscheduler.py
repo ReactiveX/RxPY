c8d41c7e7db906a778d7b2ad056a49e118c9d80b
Romain Picard <romain.picard@oakbits.com>
2021-01-20 22:43:36 +0100

    ci: another change on newthread scheduler

--- tests/test_scheduler/test_newthreadscheduler.py
+++ tests/test_scheduler/test_newthreadscheduler.py
@@ -46,7 +46,7 @@ class TestNewThreadScheduler(unittest.TestCase):
 
         scheduler.schedule_relative(timedelta(milliseconds=200), action)
 
-        sleep(0.3)
+        sleep(0.4)
         assert endtime is not None
         diff = endtime - starttime
         assert diff > timedelta(milliseconds=180)
