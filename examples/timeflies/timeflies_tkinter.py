db6ce147ae87d6ba0027974e2b8bb4f094af7edb
Dag Brattli <dag@brattli.net>
2022-02-13 08:41:51 +0100

    Fix type issues with example

--- examples/timeflies/timeflies_tkinter.py
+++ examples/timeflies/timeflies_tkinter.py
@@ -13,34 +13,34 @@ def main():
     root.title("Rx for Python rocks")
     scheduler = TkinterScheduler(root)
 
-    mousemove: Subject[Event[Any]] = Subject()
+    mousemoves: Subject[Event[Any]] = Subject()
 
     frame = Frame(root, width=600, height=600)
-
-    frame.bind("<Motion>", mousemove.on_next)
+    frame.bind("<Motion>", mousemoves.on_next)
 
     text = "TIME FLIES LIKE AN ARROW"
 
-    def on_next(info: Tuple[tkinter.Label, tkinter.Event[Any], int]):
+    def on_next(info: Tuple[tkinter.Label, "Event[Frame]", int]):
         label, ev, i = info
         label.place(x=ev.x + i * 12 + 15, y=ev.y)
 
-    def handle_label(
-        label: tkinter.Label, i: int
-    ) -> Observable[Tuple[tkinter.Label, tkinter.Event[Any], int]]:
+    def label2stream(
+        label: tkinter.Label, index: int
+    ) -> Observable[Tuple[tkinter.Label, "Event[Frame]", int]]:
         label.config(dict(borderwidth=0, padx=0, pady=0))
 
-        mapper = ops.map(lambda ev: (label, ev, i))
-        delayer = ops.delay(i * 0.1)
+        return mousemoves.pipe(
+            ops.map(lambda ev: (label, ev, index)),
+            ops.delay(index * 0.1),
+        )
 
-        return mousemove.pipe(delayer, mapper)
+    def char2label(char: str) -> Label:
+        return Label(frame, text=char)
 
-    mapper = ops.map(lambda c: Label(frame, text=c))
-    labeler = ops.flat_map_indexed(handle_label)
-
-    rx.from_(text).pipe(mapper, labeler).subscribe(
-        on_next, on_error=print, scheduler=scheduler
-    )
+    rx.from_(text).pipe(
+        ops.map(char2label),
+        ops.flat_map_indexed(label2stream),
+    ).subscribe(on_next, on_error=print, scheduler=scheduler)
 
     frame.pack()
     root.mainloop()
