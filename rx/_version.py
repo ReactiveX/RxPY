f3ec070b93bd70e207d7bf3d9f739f4b5940ae37
Dag Brattli <dag@brattli.net>
2022-02-23 08:11:53 +0100

    Fix comments

--- rx/_version.py
+++ rx/_version.py
@@ -1,2 +1,2 @@
-# Please make sure the version here remains the same as in project.cfg
-__version__ = "4.0.0"
+# Please make sure the version here remains the same as in pyproject.toml
+__version__ = "4.0.0a1"
