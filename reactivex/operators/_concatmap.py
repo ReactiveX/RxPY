a86f596a07c8f2dbe969241a63b9506c2c63b65a
mat <matt@techspace.asia>
2022-12-27 05:46:05 +0800

    Concat map implementation

---
+++ reactivex/operators/_concatmap.py
@@ -0,0 +1,20 @@
+from typing import Callable, TypeVar
+
+from reactivex import Observable
+from reactivex import operators
+from reactivex.typing import Mapper
+
+_T1 = TypeVar("_T1")
+_T2 = TypeVar("_T2")
+
+
+def concatmap_(
+    project: Mapper[_T1, Observable[_T2]]
+) -> Callable[[Observable[_T1]], Observable[_T2]]:
+    def _concat_map(source: Observable[_T1]) -> Observable[_T2]:
+        return source.pipe(operators.map(project), operators.merge(max_concurrent=1))
+
+    return _concat_map
+
+
+__all__ = ["concatmap_"]
