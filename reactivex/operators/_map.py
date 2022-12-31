a115165bef37a3833ddb0528c072512d46eddca5
Dag Brattli <dag@brattli.net>
2022-12-31 12:15:08 +0100

    Fix map operator

--- reactivex/operators/_map.py
+++ reactivex/operators/_map.py
@@ -3,6 +3,7 @@ from typing import Callable, Optional, TypeVar, cast
 from reactivex import Observable, abc, compose
 from reactivex import operators as ops
 from reactivex import typing
+from reactivex.curry import curry_flip
 from reactivex.internal.basic import identity
 from reactivex.internal.utils import infinite
 from reactivex.typing import Mapper, MapperIndexed
@@ -11,48 +12,45 @@ _T1 = TypeVar("_T1")
 _T2 = TypeVar("_T2")
 
 
+@curry_flip(1)
 def map_(
-    mapper: Optional[Mapper[_T1, _T2]] = None
-) -> Callable[[Observable[_T1]], Observable[_T2]]:
+    source: Observable[_T1], mapper: Optional[Mapper[_T1, _T2]] = None
+) -> Observable[_T2]:
+    """Partially applied map operator.
+
+    Project each element of an observable sequence into a new form
+    by incorporating the element's index.
+
+    Example:
+        >>> map(source)
+
+    Args:
+        source: The observable source to transform.
+
+    Returns:
+        Returns an observable sequence whose elements are the
+        result of invoking the transform function on each element
+        of the source.
+    """
 
     _mapper = mapper or cast(Mapper[_T1, _T2], identity)
 
-    def map(source: Observable[_T1]) -> Observable[_T2]:
-        """Partially applied map operator.
+    def subscribe(
+        obv: abc.ObserverBase[_T2], scheduler: Optional[abc.SchedulerBase] = None
+    ) -> abc.DisposableBase:
+        def on_next(value: _T1) -> None:
+            try:
+                result = _mapper(value)
+            except Exception as err:  # pylint: disable=broad-except
+                obv.on_error(err)
+            else:
+                obv.on_next(result)
 
-        Project each element of an observable sequence into a new form
-        by incorporating the element's index.
+        return source.subscribe(
+            on_next, obv.on_error, obv.on_completed, scheduler=scheduler
+        )
 
-        Example:
-            >>> map(source)
-
-        Args:
-            source: The observable source to transform.
-
-        Returns:
-            Returns an observable sequence whose elements are the
-            result of invoking the transform function on each element
-            of the source.
-        """
-
-        def subscribe(
-            obv: abc.ObserverBase[_T2], scheduler: Optional[abc.SchedulerBase] = None
-        ) -> abc.DisposableBase:
-            def on_next(value: _T1) -> None:
-                try:
-                    result = _mapper(value)
-                except Exception as err:  # pylint: disable=broad-except
-                    obv.on_error(err)
-                else:
-                    obv.on_next(result)
-
-            return source.subscribe(
-                on_next, obv.on_error, obv.on_completed, scheduler=scheduler
-            )
-
-        return Observable(subscribe)
-
-    return map
+    return Observable(subscribe)
 
 
 def map_indexed_(
